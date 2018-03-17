107a15037ee5c22915bfe58bbb6a5b4ec7fea203
Wez Furlong <wez@wezfurlong.org>
2018-03-17 16:02:11 -0700

    turn on the red led

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,28 @@ extern crate cortex_m_semihosting;
 use core::fmt::Write;
 use cortex_m_semihosting::hio;
 
+use atsamd21g18a::Peripherals;
+
 fn main() {
     let mut stdout = hio::hstdout().unwrap();
     writeln!(stdout, "Hello, world!").unwrap();
+
+    let peripherals = Peripherals::take().unwrap();
+
+    // Bit mask for the red LED.  It is attached to
+    // pin A17 and is known as digital 13 in arduino land.
+    const DIGITAL_13 : u32 = 1<<17;
+
+    // Set the LED to output mode
+    peripherals.PORT.dirset0.write(|bits| unsafe {
+        bits.bits(DIGITAL_13);
+        bits
+    });
+
+    // Turn on the LED
+    peripherals.PORT.outset0.write(|bits| unsafe {
+        bits.bits(DIGITAL_13);
+        bits
+    });
+
 }
