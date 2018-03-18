557a1a133a836aff983b19f3cd715c46e90ea2d3
Wez Furlong <wez@wezfurlong.org>
2018-03-17 18:54:35 -0700

    a place to put the hal implementation

---
+++ hal/src/lib.rs
@@ -0,0 +1,8 @@
+#![no_std]
+
+pub extern crate atsamd21g18a;
+extern crate cortex_m;
+extern crate embedded_hal as hal;
+extern crate nb;
+
+pub mod gpio;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(used)]
 #![no_std]
 
-extern crate atsamd21g18a;
+extern crate atsamd21_hal;
 extern crate cortex_m;
 extern crate cortex_m_rt;
 extern crate cortex_m_semihosting;
@@ -9,7 +9,7 @@ extern crate cortex_m_semihosting;
 use core::fmt::Write;
 use cortex_m_semihosting::hio;
 
-use atsamd21g18a::Peripherals;
+use atsamd21_hal::atsamd21g18a::Peripherals;
 
 fn main() {
     let mut stdout = hio::hstdout().unwrap();
@@ -19,7 +19,7 @@ fn main() {
 
     // Bit mask for the red LED.  It is attached to
     // pin A17 and is known as digital 13 in arduino land.
-    const DIGITAL_13 : u32 = 1<<17;
+    const DIGITAL_13: u32 = 1 << 17;
 
     // Set the LED to output mode
     peripherals.PORT.dirset0.write(|bits| unsafe {
@@ -32,5 +32,4 @@ fn main() {
         bits.bits(DIGITAL_13);
         bits
     });
-
 }
