9428dbaf6e693d660719755ce857f98cc134316f
Wez Furlong <wez@wezfurlong.org>
2018-03-18 12:50:09 -0700

    Add the second bank of pins

--- hal/src/gpio.rs
+++ hal/src/gpio.rs
@@ -1,5 +1,5 @@
 use atsamd21g18a::PORT;
-use atsamd21g18a::port::{PINCFG0_, DIRCLR, DIRSET, OUTCLR, OUTSET};
+use atsamd21g18a::port::{PINCFG0_, PINCFG1_, DIRCLR, DIRSET, OUTCLR, OUTSET};
 use core::marker::PhantomData;
 use hal::digital::OutputPin;
 
@@ -50,68 +50,11 @@ pub struct PfH;
 /// Peripheral Function I
 pub struct PfI;
 
-macro_rules! port {
-    ([
-       $($PinType:ident: ($pin_ident:ident, $pin_no:expr, $pin_mode:ty),)+
-    ]) => {
+macro_rules! pin {
+    ($PinType:ident, $pin_ident:ident, $pin_no:expr, $pin_mode:ty,
+        $dirset:ident, $dirclr:ident, $pincfg:ident, $outset:ident,
+        $outclr:ident, $pinmux:ident, $out:ident, $outtgl:ident, $in:ident) => {
 
-pub struct Parts {
-    /// Opaque port reference
-    pub port: Port,
-
-    $(
-        /// Pin $pin_ident
-        pub $pin_ident: $PinType<$pin_mode>,
-    )+
-}
-
-/// Opaque port reference
-pub struct Port {
-    _0: ()
-}
-
-impl Port {
-    fn dirset0(&mut self) -> &DIRSET {
-        unsafe {
-            &(*PORT::ptr()).dirset0
-        }
-    }
-    fn dirclr0(&mut self) -> &DIRCLR {
-        unsafe {
-            &(*PORT::ptr()).dirclr0
-        }
-    }
-    fn pincfg0(&mut self) -> &[PINCFG0_; 32] {
-        unsafe {
-            &(*PORT::ptr()).pincfg0_
-        }
-    }
-    fn outset0(&mut self) -> &OUTSET {
-        unsafe {
-            &(*PORT::ptr()).outset0
-        }
-    }
-    fn outclr0(&mut self) -> &OUTCLR {
-        unsafe {
-            &(*PORT::ptr()).outclr0
-        }
-    }
-}
-
-impl GpioExt for PORT {
-    type Parts = Parts;
-
-    fn split(self) -> Parts {
-        Parts {
-            port: Port {_0: ()},
-            $(
-                $pin_ident: $PinType { _mode: PhantomData },
-            )+
-        }
-    }
-}
-
-$(
     pub struct $PinType<MODE> {
         _mode: PhantomData<MODE>,
     }
@@ -122,12 +65,12 @@ $(
             self,
             port: &mut Port
         ) -> $PinType<Input<Floating>> {
-            port.dirclr0().write(|bits| unsafe {
+            port.$dirclr().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
 
-            port.pincfg0()[$pin_no].write(|bits| {
+            port.$pincfg()[$pin_no].write(|bits| {
                 bits.pmuxen().clear_bit();
                 bits.inen().set_bit();
                 bits.pullen().clear_bit();
@@ -143,12 +86,12 @@ $(
             self,
             port: &mut Port
         ) -> $PinType<Input<PullDown>> {
-            port.dirclr0().write(|bits| unsafe {
+            port.$dirclr().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
 
-            port.pincfg0()[$pin_no].write(|bits| {
+            port.$pincfg()[$pin_no].write(|bits| {
                 bits.pmuxen().clear_bit();
                 bits.inen().set_bit();
                 bits.pullen().set_bit();
@@ -157,7 +100,7 @@ $(
             });
 
             // Pull down
-            port.outclr0().write(|bits| unsafe {
+            port.$outclr().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
@@ -170,12 +113,12 @@ $(
             self,
             port: &mut Port
         ) -> $PinType<Input<PullUp>> {
-            port.dirclr0().write(|bits| unsafe {
+            port.$dirclr().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
 
-            port.pincfg0()[$pin_no].write(|bits| {
+            port.$pincfg()[$pin_no].write(|bits| {
                 bits.pmuxen().clear_bit();
                 bits.inen().set_bit();
                 bits.pullen().set_bit();
@@ -184,7 +127,7 @@ $(
             });
 
             // Pull up
-            port.outset0().write(|bits| unsafe {
+            port.$outset().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
@@ -197,12 +140,12 @@ $(
             self,
             port: &mut Port
         ) -> $PinType<Output<OpenDrain>> {
-            port.dirset0().write(|bits| unsafe {
+            port.$dirset().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
 
-            port.pincfg0()[$pin_no].write(|bits| {
+            port.$pincfg()[$pin_no].write(|bits| {
                 bits.pmuxen().clear_bit();
                 bits.inen().clear_bit();
                 bits.pullen().clear_bit();
@@ -218,12 +161,12 @@ $(
             self,
             port: &mut Port
         ) -> $PinType<Output<PushPull>> {
-            port.dirset0().write(|bits| unsafe {
+            port.$dirset().write(|bits| unsafe {
                 bits.bits(1 << $pin_no);
                 bits
             });
 
-            port.pincfg0()[$pin_no].write(|bits| {
+            port.$pincfg()[$pin_no].write(|bits| {
                 bits.pmuxen().clear_bit();
                 bits.inen().set_bit();
                 bits.pullen().clear_bit();
@@ -239,7 +182,7 @@ $(
     impl $PinType<Output<OpenDrain>> {
         /// Control state of the internal pull up
         pub fn internal_pull_up(&mut self, port: &mut Port, on: bool) {
-            port.pincfg0()[$pin_no].write(|bits| {
+            port.$pincfg()[$pin_no].write(|bits| {
                 if on {
                     bits.pullen().set_bit();
                 } else {
@@ -255,7 +198,7 @@ $(
         // trait: https://github.com/japaric/embedded-hal/pull/44
         pub fn toggle(&mut self) {
             unsafe {
-                (*PORT::ptr()).outtgl0.write(|bits| {
+                (*PORT::ptr()).$outtgl.write(|bits| {
                     bits.bits(1 << $pin_no);
                     bits
                 });
@@ -266,17 +209,19 @@ $(
     impl<MODE> OutputPin for $PinType<Output<MODE>> {
         fn is_high(&self) -> bool {
             unsafe {
-                (((*PORT::ptr()).out0.read().bits()) & (1<<$pin_no)) != 0
+                (((*PORT::ptr()).$out.read().bits()) & (1<<$pin_no)) != 0
             }
         }
 
         fn is_low(&self) -> bool {
-            !self.is_high()
+            unsafe {
+                (((*PORT::ptr()).$out.read().bits()) & (1<<$pin_no)) == 0
+            }
         }
 
         fn set_high(&mut self) {
             unsafe {
-                (*PORT::ptr()).outset0.write(|bits| {
+                (*PORT::ptr()).$outset.write(|bits| {
                     bits.bits(1 << $pin_no);
                     bits
                 });
@@ -285,14 +230,103 @@ $(
 
         fn set_low(&mut self) {
             unsafe {
-                (*PORT::ptr()).outclr0.write(|bits| {
+                (*PORT::ptr()).$outclr.write(|bits| {
                     bits.bits(1 << $pin_no);
                     bits
                 });
             }
         }
     }
+
+    };
+}
+
+/// Opaque port reference
+pub struct Port {
+    _0: (),
+}
+
+impl Port {
+    fn dirset0(&mut self) -> &DIRSET {
+        unsafe { &(*PORT::ptr()).dirset0 }
+    }
+    fn dirclr0(&mut self) -> &DIRCLR {
+        unsafe { &(*PORT::ptr()).dirclr0 }
+    }
+    fn pincfg0(&mut self) -> &[PINCFG0_; 32] {
+        unsafe { &(*PORT::ptr()).pincfg0_ }
+    }
+    fn outset0(&mut self) -> &OUTSET {
+        unsafe { &(*PORT::ptr()).outset0 }
+    }
+    fn outclr0(&mut self) -> &OUTCLR {
+        unsafe { &(*PORT::ptr()).outclr0 }
+    }
+
+    fn dirset1(&mut self) -> &DIRSET {
+        unsafe { &(*PORT::ptr()).dirset1 }
+    }
+    fn dirclr1(&mut self) -> &DIRCLR {
+        unsafe { &(*PORT::ptr()).dirclr1 }
+    }
+    fn pincfg1(&mut self) -> &[PINCFG1_; 32] {
+        unsafe { &(*PORT::ptr()).pincfg1_ }
+    }
+    fn outset1(&mut self) -> &OUTSET {
+        unsafe { &(*PORT::ptr()).outset1 }
+    }
+    fn outclr1(&mut self) -> &OUTCLR {
+        unsafe { &(*PORT::ptr()).outclr1 }
+    }
+}
+
+macro_rules! port {
+    ([
+       $($PinTypeA:ident: ($pin_identA:ident, $pin_noA:expr, $pin_modeA:ty),)+
+    ],[
+       $($PinTypeB:ident: ($pin_identB:ident, $pin_noB:expr, $pin_modeB:ty),)+
+    ]) => {
+
+pub struct Parts {
+    /// Opaque port reference
+    pub port: Port,
+
+    $(
+        /// Pin $pin_identA
+        pub $pin_identA: $PinTypeA<$pin_modeA>,
+    )+
+    $(
+        /// Pin $pin_identB
+        pub $pin_identB: $PinTypeB<$pin_modeB>,
+    )+
+}
+
+impl GpioExt for PORT {
+    type Parts = Parts;
+
+    /// Split the PORT peripheral into discrete pins
+    fn split(self) -> Parts {
+        Parts {
+            port: Port {_0: ()},
+            $(
+                $pin_identA: $PinTypeA { _mode: PhantomData },
+            )+
+            $(
+                $pin_identB: $PinTypeB { _mode: PhantomData },
+            )+
+        }
+    }
+}
+
+$(
+    pin!($PinTypeA, $pin_identA, $pin_noA, $pin_modeA, dirset0, dirclr0,
+        pincfg0, outset0, outclr0, pmux0, out0, outtgl0, in0);
 )+
+$(
+    pin!($PinTypeB, $pin_identB, $pin_noB, $pin_modeB, dirset1, dirclr1,
+        pincfg1, outset1, outclr1, pmux1, out1, outtgl1, in1);
+)+
+
     };
 }
 
@@ -329,4 +363,37 @@ port!([
     Pa29: (pa29, 29, Input<Floating>),
     Pa30: (pa30, 30, Input<Floating>),
     Pa31: (pa31, 31, Input<Floating>),
+],[
+    Pb0: (pb0, 0, Input<Floating>),
+    Pb1: (pb1, 1, Input<Floating>),
+    Pb2: (pb2, 2, Input<Floating>),
+    Pb3: (pb3, 3, Input<Floating>),
+    Pb4: (pb4, 4, Input<Floating>),
+    Pb5: (pb5, 5, Input<Floating>),
+    Pb6: (pb6, 6, Input<Floating>),
+    Pb7: (pb7, 7, Input<Floating>),
+    Pb8: (pb8, 8, Input<Floating>),
+    Pb9: (pb9, 9, Input<Floating>),
+    Pb10: (pb10, 10, Input<Floating>),
+    Pb11: (pb11, 11, Input<Floating>),
+    Pb12: (pb12, 12, Input<Floating>),
+    Pb13: (pb13, 13, Input<Floating>),
+    Pb14: (pb14, 14, Input<Floating>),
+    Pb15: (pb15, 15, Input<Floating>),
+    Pb16: (pb16, 16, Input<Floating>),
+    Pb17: (pb17, 17, Input<Floating>),
+    Pb18: (pb18, 18, Input<Floating>),
+    Pb19: (pb19, 19, Input<Floating>),
+    Pb20: (pb20, 20, Input<Floating>),
+    Pb21: (pb21, 21, Input<Floating>),
+    Pb22: (pb22, 22, Input<Floating>),
+    Pb23: (pb23, 23, Input<Floating>),
+    Pb24: (pb24, 24, Input<Floating>),
+    Pb25: (pb25, 25, Input<Floating>),
+    Pb26: (pb26, 26, Input<Floating>),
+    Pb27: (pb27, 27, Input<Floating>),
+    Pb28: (pb28, 28, Input<Floating>),
+    Pb29: (pb29, 29, Input<Floating>),
+    Pb30: (pb30, 30, Input<Floating>),
+    Pb31: (pb31, 31, Input<Floating>),
 ]);
