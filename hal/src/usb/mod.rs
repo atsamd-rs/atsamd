3e54f65b4cd01d4cbbddfd9b07f76a2155de74e5
Wez Furlong <wez@wezfurlong.org>
2018-04-28 20:33:33 -0700

    add some more usb init code

--- hal/src/usb/mod.rs
+++ hal/src/usb/mod.rs
@@ -1,7 +1,10 @@
 //! USB Device support
 
+use atsamd21g18a::usb::DEVICE;
 use atsamd21g18a::{PM, USB};
+use calibration::{usb_transn_cal, usb_transp_cal, usb_trim_cal};
 use clock;
+use core::ptr::null_mut;
 use gpio;
 
 /// Emit SOF at 1Khz on this pin when configured as function G
@@ -13,13 +16,14 @@ pub type DmPad = gpio::Pa24<gpio::PfG>;
 /// USB D+ is connected here
 pub type DpPad = gpio::Pa25<gpio::PfG>;
 
-pub struct UsbDevice {
+pub struct UsbDevice<'a> {
+    desc: &'a mut Descriptors,
     dm_pad: DmPad,
     dp_pad: DpPad,
     usb: USB,
 }
 
-#[repr(C)]
+#[repr(packed)]
 pub struct DeviceDescBank {
     /// endpoint data buffer, must be 32-bit aligned
     addr: *mut u8,
@@ -30,6 +34,16 @@ pub struct DeviceDescBank {
 }
 
 impl DeviceDescBank {
+    fn new() -> Self {
+        Self {
+            addr: null_mut(),
+            pcksize: 0,
+            extreg: 0,
+            status_bk: 0,
+            _reserved: [0, 0, 0, 0, 0],
+        }
+    }
+
     /// This bit defines the automatic Zero Length Packet mode of the endpoint.
     /// When enabled, the USB module will manage the ZLP handshake by hardware.
     /// This bit is for IN endpoints only. When disabled the handshake should be
@@ -110,26 +124,84 @@ impl DeviceDescBank {
     }
 }
 
-#[repr(C)]
+#[repr(packed)]
 pub struct DeviceDescriptor {
     bank: [DeviceDescBank; 2],
 }
 
-impl UsbDevice {
+impl DeviceDescriptor {
+    fn new() -> Self {
+        Self {
+            bank: [DeviceDescBank::new(), DeviceDescBank::new()],
+        }
+    }
+}
+
+#[repr(packed)]
+pub struct Descriptors {
+    desc: [DeviceDescriptor; 8],
+}
+
+impl Descriptors {
+    pub fn new() -> Self {
+        Self {
+            desc: [
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+                DeviceDescriptor::new(),
+            ],
+        }
+    }
+}
+
+impl<'a> UsbDevice<'a> {
     pub fn new(
         _clock: &clock::UsbClock,
         pm: &mut PM,
         dm_pad: DmPad,
         dp_pad: DpPad,
         usb: USB,
+        desc: &'a mut Descriptors,
     ) -> Self {
         pm.apbbmask.modify(|_, w| w.usb_().set_bit());
-
         Self {
             dm_pad,
             dp_pad,
             usb,
+            desc,
         }
+    }
+
+    fn usb(&self) -> &DEVICE {
+        unsafe { &self.usb.device }
+    }
+
+    /// Reset the USB hardware.
+    pub fn reset(&mut self) {
+        let addr = self.desc as *mut _ as u32;
+        let usb = self.usb();
+        usb.ctrla.modify(|_, w| w.swrst().set_bit());
+        while usb.syncbusy.read().swrst().bit_is_set() {}
+
+        usb.descadd.write(|w| unsafe { w.descadd().bits(addr) });
+        usb.padcal.modify(|_, w| unsafe {
+            w.transn().bits(usb_transn_cal());
+            w.transp().bits(usb_transp_cal());
+            w.trim().bits(usb_trim_cal())
+        });
+        usb.ctrla.modify(|_, w| {
+            w.mode().device();
+            w.runstdby().set_bit()
+        });
+        // full speed
+        usb.ctrlb.modify(|_, w| w.spdconf().fs());
+
+        usb.ctrla.modify(|_, w| w.enable().set_bit());
     }
 
     pub fn free(self) -> (DmPad, DpPad, USB) {
