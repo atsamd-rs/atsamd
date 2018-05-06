b5f8a14c52598651c3f0475a87eb97b075597625
Wez Furlong <wez@wezfurlong.org>
2018-05-06 09:47:11 -0700

    split sercom into sub-modules

---
+++ hal/src/sercom/mod.rs
@@ -0,0 +1,15 @@
+//! Working with the SERCOM peripherals.
+//!
+//! The atsamd21 hardware has several SERCOM instances that can
+//! be configured to perform a variety of serial communication
+//! tasks.  This configuration is expressed through the use of
+//! type states to make it difficult to misuse.
+//! Each sercom instance is associated with a group of IO pins
+//! referred to as a Pad.  When the pins are set to the appropriate
+//! peripheral function mode they are routed to the sercom pad.
+
+mod i2c;
+mod pads;
+
+pub use self::i2c::*;
+pub use self::pads::*;
