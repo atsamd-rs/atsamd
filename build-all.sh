b70b1fcddca7c64d16e2d4a3d9f2e189891dac65
Wez Furlong <wez@wezfurlong.org>
2018-09-14 22:14:56 -0700

    move boards to a boards subdir

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -xe
 
-cargo build --manifest-path metro_m0/Cargo.toml --example blinky_basic
-cargo build --manifest-path gemma_m0/Cargo.toml --examples
-cargo build --manifest-path samd21_mini/Cargo.toml --examples
-cargo build --manifest-path arduino_mkrzero/Cargo.toml --examples
+# Why no workspace?  Each board has a different memory.x and in
+# some cases, different core device, so building in a workspace
+# will try to link the wrong pieces together.  We have to build
+# each board as its own entity.
+
+cargo build --manifest-path boards/metro_m0/Cargo.toml --example blinky_basic
+cargo build --manifest-path boards/gemma_m0/Cargo.toml --examples
+cargo build --manifest-path boards/samd21_mini/Cargo.toml --examples
+cargo build --manifest-path boards/arduino_mkrzero/Cargo.toml --examples
