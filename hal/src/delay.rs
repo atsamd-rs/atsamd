545204143dc2f370fb2c481877f77c33e122c7ad
James Munns <james@onevariable.com>
2018-10-11 00:41:11 +0200

    Implement improved Delay, covering full u32 range
    Based on @gbip's impl in https://github.com/therealprof/stm32f4xx-hal

--- hal/src/delay.rs
+++ hal/src/delay.rs
@@ -50,17 +50,29 @@ impl DelayMs<u8> for Delay {
 
 impl DelayUs<u32> for Delay {
     fn delay_us(&mut self, us: u32) {
-        let rvr = us * (self.sysclock.0 / 1_000_000);
+        // The SysTick Reload Value register supports values between 1 and 0x00FFFFFF.
+        const MAX_RVR: u32 =  0x00FF_FFFF;
 
-        assert!(rvr < (1 << 24));
+        let mut total_rvr = us * (self.sysclock.0 / 1_000_000);
 
-        self.syst.set_reload(rvr);
-        self.syst.clear_current();
-        self.syst.enable_counter();
+        while total_rvr != 0 {
+            let current_rvr = if total_rvr <= MAX_RVR {
+                total_rvr
+            } else {
+                MAX_RVR
+            };
 
-        while !self.syst.has_wrapped() {}
+            self.syst.set_reload(current_rvr);
+            self.syst.clear_current();
+            self.syst.enable_counter();
 
-        self.syst.disable_counter();
+            // Update the tracking variable while we are waiting...
+            total_rvr -= current_rvr;
+
+            while !self.syst.has_wrapped() {}
+
+            self.syst.disable_counter();
+        }
     }
 }
 
