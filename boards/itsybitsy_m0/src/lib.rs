51a0eb9684698bc2d8f15bbf5b678e856b370466
Ben Bergman <ben@benbergman.ca>
2018-10-12 13:53:51 -0500

    Added some more ItsyBitsy pins and docs

--- boards/itsybitsy_m0/src/lib.rs
+++ boards/itsybitsy_m0/src/lib.rs
@@ -20,7 +20,24 @@ define_pins!(
     struct Pins,
     target_device: atsamd21g18a,
 
+    /// Analog pin 0.  Can act as a true analog output
+    /// as it has a DAC (which is not currently supported
+    /// by this hal) as well as input.
+    pin a0 = a2,
+    /// Analog Pin 1
+    pin a1 = b8,
+    /// Analog Pin 2
+    pin a2 = b9,
+    /// Analog Pin 3
+    pin a3 = a4,
+    /// Analog Pin 4
+    pin a4 = a5,
+    /// Analog Pin 5
+    pin a5 = b2,
+
+    /// Pin 0, rx
     pin d0 = a11,
+    /// Pin 1, tx
     pin d1 = a10,
     pin d2 = a14,
     pin d3 = a9,
@@ -33,27 +50,41 @@ define_pins!(
     pin d10 = a18,
     pin d11 = a16,
     pin d12 = a19,
+    /// Digital pin number 13, which is also attached to
+    /// the red LED.  PWM capable.
     pin d13 = a17,
 
-    pin a0 = a2,
-    pin a1 = b8,
-    pin a2 = b9,
-    pin a3 = a4,
-    pin a4 = a5,
-    pin a5 = b2,
-
+    /// The SPI MOSI attached the to 2x3 header
     pin mosi = b10,
+    /// The SPI MISO attached the to 2x3 header
     pin miso = a12,
+    /// The SPI SCK attached the to 2x3 header
     pin sck = b11,
 
+    /// The MOSI pin attached to the on-board SPI flash
     pin flash_mosi = b22,
+    /// The MISO pin attached to the on-board SPI flash
     pin flash_miso = b3,
+    /// The SCK pin attached to the on-board SPI flash
     pin flash_sck = b23,
+    /// The CS pin attached to the on-board SPI flash
     pin flash_cs = a27,
 
+    /// The I2C clock
     pin scl = a23,
+    /// The I2C data line
     pin sda = a22,
+
+    /// The DotStar clock
+    pin dotstar_ci = a0,
+    /// The DotStar data line
+    pin dotstar_di = a1,
 
     pin swdio = a31,
     pin swdclk = a30,
+
+    /// The USB D- pad
+    pin usb_dm = a24,
+    /// The USB D+ pad
+    pin usb_dp = a25,
 );
