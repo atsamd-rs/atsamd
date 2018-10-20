56021d466d8f12b58d1c25b3d4687f0a5d755440
Wez Furlong <wez@wezfurlong.org>
2018-10-20 10:11:02 -0700

    fix typo and get travis happy

--- build-all.sh
+++ build-all.sh
@@ -7,7 +7,7 @@ set -xe
 # each board as its own entity.
 
 cargo build --manifest-path boards/metro_m0/Cargo.toml --example blinky_basic
-cargo build --manifest-path boards/feather_m0/Cargo.toml --example
+cargo build --manifest-path boards/feather_m0/Cargo.toml --examples
 cargo build --manifest-path boards/gemma_m0/Cargo.toml --examples
 cargo build --manifest-path boards/itsybitsy_m0/Cargo.toml --examples
 cargo build --manifest-path boards/trinket_m0/Cargo.toml --examples
