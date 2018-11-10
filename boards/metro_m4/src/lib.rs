d3ddb650af3cb1361a2bc5a3ad8222c536a53749
Paul Sajna <paulsajna@gmail.com>
2018-11-09 17:13:17 -0800

    update metro m4 board def

--- boards/metro_m4/src/lib.rs
+++ boards/metro_m4/src/lib.rs
@@ -14,7 +14,7 @@ pub use hal::*;
 
 use gpio::{Floating, Input, IntoFunction, Output, Port, PushPull};
 use hal::clock::GenericClockController;
-use hal::sercom::{I2CMaster3, PadPin, SPIMaster4, SPIMaster5};
+use hal::sercom::{I2CMaster5, PadPin, SPIMaster0, SPIMaster2};
 use hal::time::Hertz;
 
 #[cfg(feature = "usb")]
@@ -34,67 +34,67 @@ define_pins!(
     pin a0 = a2,
 
     /// Analog Pin 1
-    pin a1 = b8,
+    pin a1 = a5,
     /// Analog Pin 2
-    pin a2 = b9,
+    pin a2 = a6,
     /// Analog Pin 3
     pin a3 = a4,
     /// Analog Pin 4
-    pin a4 = a5,
+    pin a4 = b8,
     /// Analog Pin 5
-    pin a5 = b2,
+    pin a5 = b9,
 
-    /// Pin 0, rx.  Also analog input (A6)
-    pin d0 = a11,
-    /// Pin 1, tx.  Also analog input (A7)
-    pin d1 = a10,
+    /// Pin 0, rx.
+    pin d0 = a23,
+    /// Pin 1, tx.
+    pin d1 = a22,
     /// Pin 2
-    pin d2 = a14,
-    /// Pin 3, PWM capable
-    pin d3 = a9,
-    /// Pin 4, PWM capable.  Also analog input (A8)
-    pin d4 = a8,
-    /// Pin 5, PWM capable.  Also analog input (A9)
-    pin d5 = a15,
-    /// Pin 6, PWM capable
-    pin d6 = a20,
+    pin d2 = b17,
+    /// Pin 3 
+    pin d3 = b16,
+    /// Pin 4
+    pin d4 = b13,
+    /// Pin 5
+    pin d5 = b14,
+    /// Pin 6
+    pin d6 = b15,
     /// Pin 7
-    pin d7 = a21,
-    /// Pin 8, PWM capable.  Also analog input (A10)
-    pin d8 = a6,
-    /// Pin 9, PWM capable.  Also analog input (A11)
-    pin d9 = a7,
-    /// Pin 10, PWM capable
+    pin d7 = b12,
+    /// Pin 8
+    pin d8 = a21,
+    /// Pin 9
+    pin d9 = a20,
+    /// Pin 10
     pin d10 = a18,
-    /// Pin 11, PWM capable
-    pin d11 = a16,
-    /// Pin 12, PWM capable
-    pin d12 = a19,
+    /// Pin 11
+    pin d11 = a19,
+    /// Pin 12
+    pin d12 = a17,
     /// Digital pin number 13, which is also attached to
     /// the red LED.  PWM capable.
-    pin d13 = a17,
-    pin sda = a22,
-    pin scl = a23,
+    pin d13 = a16,
+    pin sda = b2,
+    pin scl = b3,
 
     /// The data line attached to the neopixel.
     /// Is also attached to SWCLK.
-    pin neopixel = a30,
+    pin neopixel = b22,
 
     /// The SPI SCK attached the to 2x3 header
-    pin sck = b11,
+    pin sck = a13,
     /// The SPI MOSI attached the to 2x3 header
-    pin mosi = b10,
+    pin mosi = a12,
     /// The SPI MISO attached the to 2x3 header
-    pin miso = a12,
+    pin miso = a14,
 
     /// The SCK pin attached to the on-board SPI flash
-    pin flash_sck = b23,
+    pin flash_sck = b10,
     /// The MOSI pin attached to the on-board SPI flash
-    pin flash_mosi = b22,
+    pin flash_mosi = a8,
     /// The MISO pin attached to the on-board SPI flash
-    pin flash_miso = b3,
+    pin flash_miso = a9,
     /// The CS pin attached to the on-board SPI flash
-    pin flash_cs = a13,
+    pin flash_cs = b11,
 
     /// The USB D- pad
     pin usb_dm = a24,
@@ -103,7 +103,7 @@ define_pins!(
 );
 
 /// Convenience for setting up the 2x3 header block for SPI.
-/// This powers up SERCOM4 and configures it for use as an
+/// This powers up SERCOM2 and configures it for use as an
 /// SPI Master in SPI Mode 0.
 /// Unlike the `flash_spi_master` function, this
 /// one does not accept a CS pin; configuring a pin for CS
@@ -112,24 +112,24 @@ define_pins!(
 pub fn spi_master<F: Into<Hertz>>(
     clocks: &mut GenericClockController,
     bus_speed: F,
-    sercom4: SERCOM4,
+    sercom2: SERCOM2,
     mclk: &mut MCLK,
-    sck: gpio::Pb11<Input<Floating>>,
-    mosi: gpio::Pb10<Input<Floating>>,
-    miso: gpio::Pa12<Input<Floating>>,
+    sck: gpio::Pa13<Input<Floating>>,
+    mosi: gpio::Pa12<Input<Floating>>,
+    miso: gpio::Pa14<Input<Floating>>,
     port: &mut Port,
-) -> SPIMaster4 {
+) -> SPIMaster2 {
     let gclk0 = clocks.gclk0();
-    SPIMaster4::new(
-        &clocks.sercom4_core(&gclk0).unwrap(),
+    SPIMaster2::new(
+        &clocks.sercom2_core(&gclk0).unwrap(),
         bus_speed.into(),
         hal::hal::spi::Mode {
             phase: hal::hal::spi::Phase::CaptureOnFirstTransition,
             polarity: hal::hal::spi::Polarity::IdleLow,
         },
-        sercom4,
+        sercom2,
         mclk,
-        hal::sercom::SPI4Pinout::Dipo0Dopo1 {
+        hal::sercom::SPI2Pinout::Dipo2Dopo0 {
             miso: miso.into_pad(port),
             mosi: mosi.into_pad(port),
             sck: sck.into_pad(port),
@@ -137,82 +137,63 @@ pub fn spi_master<F: Into<Hertz>>(
     )
 }
 
-/// Convenience for accessing the on-board SPI Flash device.
-/// This powers up SERCOM5 and configures it for use as an
-/// SPI Master.
-pub fn flash_spi_master(
-    clocks: &mut GenericClockController,
-    sercom5: SERCOM5,
-    mclk: &mut MCLK,
-    sck: gpio::Pb23<Input<Floating>>,
-    mosi: gpio::Pb22<Input<Floating>>,
-    miso: gpio::Pb3<Input<Floating>>,
-    cs: gpio::Pa13<Input<Floating>>,
-    port: &mut Port,
-) -> (SPIMaster5, gpio::Pa13<Output<PushPull>>) {
-    let gclk0 = clocks.gclk0();
-    let flash = SPIMaster5::new(
-        &clocks.sercom5_core(&gclk0).unwrap(),
-        48.mhz(),
-        hal::hal::spi::Mode {
-            phase: hal::hal::spi::Phase::CaptureOnFirstTransition,
-            polarity: hal::hal::spi::Polarity::IdleLow,
-        },
-        sercom5,
-        mclk,
-        hal::sercom::SPI5Pinout::Dipo1Dopo1 {
-            miso: miso.into_pad(port),
-            mosi: mosi.into_pad(port),
-            sck: sck.into_pad(port),
-        },
-    );
-
-    let mut cs = cs.into_push_pull_output(port);
-    cs.set_high();
-
-    (flash, cs)
-}
+// Convenience for accessing the on-board SPI Flash device.
+// This is commented out because it should use the QSPI peripheral, not a sercom,
+// so we will need to add hal around the QSPI peripheral if we want this feature.
+//
+/*
+ *pub fn flash_spi_master(
+ *    clocks: &mut GenericClockController,
+ *    sercom0: SERCOM0,
+ *    mclk: &mut MCLK,
+ *    sck: gpio::Pb10<Input<Floating>>,
+ *    mosi: gpio::Pa8<Input<Floating>>,
+ *    miso: gpio::Pa9<Input<Floating>>,
+ *    cs: gpio::Pb11<Input<Floating>>,
+ *    port: &mut Port,
+ *) -> (SPIMaster0, gpio::Pb11<Output<PushPull>>) {
+ *    let gclk0 = clocks.gclk0();
+ *    let flash = SPIMaster0::new(
+ *        &clocks.sercom0_core(&gclk0).unwrap(),
+ *        48.mhz(),
+ *        hal::hal::spi::Mode {
+ *            phase: hal::hal::spi::Phase::CaptureOnFirstTransition,
+ *            polarity: hal::hal::spi::Polarity::IdleLow,
+ *        },
+ *        sercom0,
+ *        mclk,
+ *        hal::sercom::SPI0Pinout::Dipo1Dopo3 {
+ *            miso: miso.into_pad(port),
+ *            mosi: mosi.into_pad(port),
+ *            sck: sck.into_pad(port),
+ *        },
+ *    );
+ *
+ *    let mut cs = cs.into_push_pull_output(port);
+ *    cs.set_high();
+ *
+ *    (flash, cs)
+ *}
+ */
 
 /// Convenience for setting up the labelled SDA, SCL pins to
 /// operate as an I2C master running at the specified frequency.
 pub fn i2c_master<F: Into<Hertz>>(
     clocks: &mut GenericClockController,
     bus_speed: F,
-    sercom3: SERCOM3,
+    sercom5: SERCOM5,
     mclk: &mut MCLK,
-    sda: gpio::Pa22<Input<Floating>>,
-    scl: gpio::Pa23<Input<Floating>>,
+    sda: gpio::Pb2<Input<Floating>>,
+    scl: gpio::Pb3<Input<Floating>>,
     port: &mut Port,
-) -> I2CMaster3 {
+) -> I2CMaster5 {
     let gclk0 = clocks.gclk0();
-    I2CMaster3::new(
-        &clocks.sercom3_core(&gclk0).unwrap(),
+    I2CMaster5::new(
+        &clocks.sercom5_core(&gclk0).unwrap(),
         bus_speed.into(),
-        sercom3,
+        sercom5,
         mclk,
         sda.into_pad(port),
         scl.into_pad(port),
     )
-}
-
-#[cfg(feature = "usb")]
-pub fn usb_bus(
-    usb: USB,
-    clocks: &mut GenericClockController,
-    mclk: &mut MCLK,
-    dm: gpio::Pa24<Input<Floating>>,
-    dp: gpio::Pa25<Input<Floating>>,
-    port: &mut Port,
-) -> UsbBusWrapper<UsbBus> {
-    let gclk0 = clocks.gclk0();
-    dbgprint!("making usb clock");
-    let usb_clock = &clocks.usb(&gclk0).unwrap();
-    dbgprint!("got clock");
-    UsbBusWrapper::new(UsbBus::new(
-        usb_clock,
-        mclk,
-        dm.into_function(port),
-        dp.into_function(port),
-        usb,
-    ))
 }
