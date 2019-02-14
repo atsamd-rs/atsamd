d311b3c4ce465c881ac769c1a3995e98e672deb9
Paul Sajna <sajattack@gmail.com>
2019-02-13 16:10:40 -0800

    add metro_m4 to build script

--- build-docs.py
+++ build-docs.py
@@ -24,7 +24,9 @@ crates_by_pac = {
         'samd21_mini',
         'sodaq_one',
     ],
-    'atsamd51j19a': [],
+    'atsamd51j19a': [
+        'metro_m4',     
+    ],
 }
 
 def copy_skeleton_crate(src_dir, dest_dir):
