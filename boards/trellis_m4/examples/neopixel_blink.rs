4576432d6cf30a8f12f520b7597666807c414049
Tony Arcieri <bascule@gmail.com>
2019-03-10 08:12:25 -0700

    trellis_m4: use ws2812-nop-samd51, disable lto
    We measured ws2812-nop-samd51's bitbanged signal to the NeoPixels  with
    a logic analyzer and discovered it was going too fast with LTO enabled
    (which was needed originally to get the timer-driven examples working).
    
    With LTO off ws2812-nop-samd51 can *correctly* drive the NeoPixels,
    and the examples no longer run much too fast like they did with the
    timer-driven approach.

--- boards/trellis_m4/examples/neopixel_blink.rs
+++ boards/trellis_m4/examples/neopixel_blink.rs
@@ -2,53 +2,48 @@
 #![no_main]
 
 extern crate cortex_m;
-extern crate trellis_m4 as hal;
 extern crate panic_halt;
 extern crate smart_leds;
-extern crate ws2812_timer_delay as ws2812;
+extern crate trellis_m4 as hal;
+extern crate ws2812_nop_samd51 as ws2812;
 
 use hal::prelude::*;
 use hal::{entry, Peripherals, CorePeripherals};
 use hal::{clock::GenericClockController, delay::Delay};
-use hal::{timer::TimerCounter};
 
-use smart_leds::Color;
-use smart_leds::colors::RED;
 use smart_leds::brightness;
+use smart_leds::colors::RED;
+use smart_leds::Color;
 use smart_leds::SmartLedsWrite;
 
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-    let core = CorePeripherals::take().unwrap();
-    let mut clocks = GenericClockController::with_external_32kosc(
+    let core_peripherals = CorePeripherals::take().unwrap();
+
+    let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.GCLK,
         &mut peripherals.MCLK,
         &mut peripherals.OSC32KCTRL,
         &mut peripherals.OSCCTRL,
         &mut peripherals.NVMCTRL,
     );
+
     let mut pins = hal::Pins::new(peripherals.PORT);
-
-    let gclk0 = clocks.gclk0();
-    let timer_clock = clocks.tc2_tc3(&gclk0).unwrap();
-    let mut timer = TimerCounter::tc3_(
-        &timer_clock, 
-        peripherals.TC3, 
-        &mut peripherals.MCLK
-    );
-    timer.start(3_000_000u32.hz());
-
-    let mut neopixel_pin = pins.neopixel.into_push_pull_output(&mut pins.port);
-    let mut neopixel = ws2812::Ws2812::new(timer, &mut neopixel_pin);
-    let mut delay = Delay::new(core.SYST, &mut clocks);
+    let mut delay = Delay::new(core_peripherals.SYST, &mut clocks);
+    let neopixel_pin = pins.neopixel.into_push_pull_output(&mut pins.port);
+    let mut neopixel = ws2812::Ws2812::new(neopixel_pin);
 
     loop {
         let data = [RED; 1];
-        neopixel.write(brightness(data.iter().cloned(), 32)).unwrap();
+        neopixel
+            .write(brightness(data.iter().cloned(), 32))
+            .unwrap();
         delay.delay_ms(250u8);
         let data2 = [Color::default(); 1];
-        neopixel.write(brightness(data2.iter().cloned(), 32)).unwrap();
+        neopixel
+            .write(brightness(data2.iter().cloned(), 32))
+            .unwrap();
         delay.delay_ms(250u8);
     }
 }
