ec979bbf12ecac55558f7217fb918a76b923dd8e
Tony Arcieri <bascule@gmail.com>
2019-03-11 17:21:31 -0700

    trellis_m4 keypad-unproven feature
    Adds keypad support using the `keypad` crate.
    
    Requires use of the `unproven` feature, so it's disabled-by-default
    and gated under the `keypad-unproven` feature.

--- boards/trellis_m4/src/lib.rs
+++ boards/trellis_m4/src/lib.rs
@@ -6,6 +6,10 @@ extern crate atsamd_hal as hal;
 #[cfg(feature = "rt")]
 extern crate cortex_m_rt;
 
+#[cfg(feature = "keypad-unproven")]
+#[macro_use]
+extern crate keypad;
+
 pub mod pins;
 
 #[cfg(feature = "rt")]
@@ -18,6 +22,8 @@ use hal::prelude::*;
 use hal::clock::GenericClockController;
 use hal::sercom::{I2CMaster4, PadPin};
 use hal::time::Hertz;
+#[cfg(feature = "keypad-unproven")]
+use hal::gpio::{OpenDrain, Output, PullUp};
 
 define_pins!(
     /// Maps the pins to their arduino names and
@@ -156,6 +162,54 @@ impl Pins {
             neopixel,
             port,
         }
+    }
+}
+
+#[cfg(feature = "keypad-unproven")]
+keypad_struct! {
+    #[doc="NeoTrellis M4 Express 8x4 keypad"]
+    pub struct Keypad {
+        rows: (
+            gpio::Pa18<Input<PullUp>>,
+            gpio::Pa19<Input<PullUp>>,
+            gpio::Pb22<Input<PullUp>>,
+            gpio::Pb23<Input<PullUp>>,
+        ),
+        columns: (
+            gpio::Pa14<Output<OpenDrain>>,
+            gpio::Pa15<Output<OpenDrain>>,
+            gpio::Pa16<Output<OpenDrain>>,
+            gpio::Pa17<Output<OpenDrain>>,
+            gpio::Pa20<Output<OpenDrain>>,
+            gpio::Pa21<Output<OpenDrain>>,
+            gpio::Pa22<Output<OpenDrain>>,
+            gpio::Pa23<Output<OpenDrain>>,
+        ),
+    }
+}
+
+#[cfg(feature = "keypad-unproven")]
+impl Keypad {
+    /// Create a new Keypad from the given pins
+    pub fn new(pins: pins::Keypad, port: &mut Port) -> Self {
+        keypad_new!(Self {
+            rows: (
+                pins.row0.into_pull_up_input(port),
+                pins.row1.into_pull_up_input(port),
+                pins.row2.into_pull_up_input(port),
+                pins.row3.into_pull_up_input(port),
+            ),
+            columns: (
+                pins.col0.into_open_drain_output(port),
+                pins.col1.into_open_drain_output(port),
+                pins.col2.into_open_drain_output(port),
+                pins.col3.into_open_drain_output(port),
+                pins.col4.into_open_drain_output(port),
+                pins.col5.into_open_drain_output(port),
+                pins.col6.into_open_drain_output(port),
+                pins.col7.into_open_drain_output(port),
+            ),
+        })
     }
 }
 
