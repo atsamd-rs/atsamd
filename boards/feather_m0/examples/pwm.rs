4197ec7c6490f1823be9b2f0f51defa7e9ecab69
Paul Sajna <sajattack@gmail.com>
2019-04-03 15:52:42 -0700

    pwm example for feather m0 (samd21) (it works!)

---
+++ boards/feather_m0/examples/pwm.rs
@@ -0,0 +1,46 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate feather_m0 as hal;
+extern crate cortex_m_rt;
+
+use hal::clock::GenericClockController;
+use hal::{Peripherals, CorePeripherals};
+use hal::prelude::*;
+use hal::delay::Delay;
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+
+    let mut pins = hal::Pins::new(peripherals.PORT);
+    let d5 = pins.d5.into_function_e(&mut pins.port);
+    let gclk0 = clocks.gclk0();
+    let mut pwm3 = hal::pwm::Pwm3::new(
+        &clocks.tcc2_tc3(&gclk0).unwrap(),
+        1.khz(),
+        peripherals.TC3,
+        hal::pwm::TC3Pinout::Pa15(d5),
+        &mut peripherals.PM,
+    );
+
+    let max_duty = pwm3.get_max_duty();
+
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    loop {
+        pwm3.set_duty(hal::pwm::Channels::C0, max_duty/2);
+        delay.delay_ms(1000u16);
+        pwm3.set_duty(hal::pwm::Channels::C0, max_duty/8);
+        delay.delay_ms(1000u16);
+    }
+}
