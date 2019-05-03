1430e18b4194c612f80c2c04449fdab4cacb84bc
Paul Sajna <sajattack@gmail.com>
2019-05-02 17:11:21 -0700

    fix feather_m4

--- boards/feather_m4/src/lib.rs
+++ boards/feather_m4/src/lib.rs
@@ -8,7 +8,7 @@ extern crate cortex_m_rt;
 #[cfg(feature = "rt")]
 pub use cortex_m_rt::entry;
 
-pub use hal::atsamd51j19a::*;
+pub use target_device::*;
 use hal::prelude::*;
 pub use hal::*;
 
@@ -28,7 +28,7 @@ define_pins!(
     /// Maps the pins to their arduino names and
     /// the numbers printed on the board.
     struct Pins,
-    target_device: atsamd51j19a,
+    target_device: target_device, 
 
     /// Analog pin 0.  Can act as a true analog output
     /// as it has a DAC (which is not currently supported
