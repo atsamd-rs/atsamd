9f229b4ea8a3498d93de29b305bcf9c13ad3e047
Keith Duncan <keith_duncan@me.com>
2019-05-16 15:31:34 +1000

    Add type states to the sercom51 module pads

--- hal/src/sercom51/pads.rs
+++ hal/src/sercom51/pads.rs
@@ -11,204 +11,194 @@ pub trait PadPin<T> {
 /// The pad macro helps to define enums for pads and makes it
 /// a little more convenient to initialize them.
 macro_rules! pad {
-    ($(pub enum $PadType:ident {
-        $( $PinType:ident ($new:ident, $Pf:ident),)+
-    })+
+    ($PadType:ident {
+        $($PinType:ident ($Pf:ident),)+
+    }
     ) => {
-$(
-/// Represents a numbered pad for the associated sercom instance
-pub enum $PadType {
-    $(
-        $PinType(gpio::$PinType<gpio::$Pf>),
-    )+
-}
+/// Represents a numbered pad for the associated sercom instance. The pad is
+/// generic over any pin, only the PadPin implementations in this the sercom
+/// module make sense.
+pub struct $PadType<PIN>(PIN);
 
-impl $PadType {
-    $(
+impl<PIN> $PadType<PIN> {
     /// Construct pad from the appropriate pin in any mode.
     /// You may find it more convenient to use the `into_pad` trait
     /// and avoid referencing the pad type.
-    pub fn $new<MODE>(pin: gpio::$PinType<MODE>, port: &mut Port) -> Self {
-        $PadType::$PinType(pin.into_function(port))
+    pub fn new(pin: PIN) -> Self {
+        $PadType(pin)
     }
-
-    )+
 }
 
 $(
-impl<MODE> PadPin<$PadType> for gpio::$PinType<MODE> {
-    fn into_pad(self, port: &mut Port) -> $PadType {
-        $PadType::$new(self, port)
+    impl<MODE> PadPin<$PadType<gpio::$PinType<gpio::$Pf>>> for gpio::$PinType<MODE> {
+        fn into_pad(self, port: &mut Port) -> $PadType<gpio::$PinType<gpio::$Pf>> {
+            $PadType::new(self.into_function(port))
+        }
     }
-}
 )+
 
-)+
     };
 }
 
-pad!(
 // sercom0[0]:  PA04:D   PA08:C
 // sercom0[1]:  PA05:D   PA09:C
 // sercom0[2]:  PA06:D   PA10:C
 // sercom0[3]:  PA07:D   PA11:C
 
-pub enum Sercom0Pad0 {
-    Pa4(pa4, PfD),
-    Pa8(pa8, PfC),
-}
+pad!(Sercom0Pad0 {
+    Pa4(PfD),
+    Pa8(PfC),
+});
 
-pub enum Sercom0Pad1 {
-    Pa5(pa5, PfD),
-    Pa9(pa9, PfC),
-}
+pad!(Sercom0Pad1 {
+    Pa5(PfD),
+    Pa9(PfC),
+});
 
-pub enum Sercom0Pad2 {
-    Pa6(pa6, PfD),
-    Pa10(pa10, PfC),
-}
+pad!(Sercom0Pad2 {
+    Pa6(PfD),
+    Pa10(PfC),
+});
 
-pub enum Sercom0Pad3 {
-    Pa7(pa7, PfD),
-    Pa11(pa11, PfC),
-}
+pad!(Sercom0Pad3 {
+    Pa7(PfD),
+    Pa11(PfC),
+});
 
 // sercom1[0]:  PA16:C   PA00:D
 // sercom1[1]:  PA17:C   PA01:D
 // sercom1[2]:  PA18:C   PA30:D
 // sercom1[3]:  PA19:C   PA31:D
 
-pub enum Sercom1Pad0 {
-    Pa0(pa0, PfD),
-    Pa16(pa16, PfC),
-}
+pad!(Sercom1Pad0 {
+    Pa0(PfD),
+    Pa16(PfC),
+});
 
-pub enum Sercom1Pad1 {
-    Pa1(pa1, PfD),
-    Pa17(pa17, PfC),
-}
+pad!(Sercom1Pad1 {
+    Pa1(PfD),
+    Pa17(PfC),
+});
 
+pad!(Sercom1Pad2 {
+    Pa18(PfC),
+    Pa30(PfD),
+    Pb22(PfC),
+});
 
+pad!(Sercom1Pad3 {
+    Pa19(PfC),
+    Pa31(PfD),
+    Pb23(PfC),
+});
 
 // sercom2[0]:  PA12:C   PA08:D
 // sercom2[1]:  PA13:C   PA09:D
 // sercom2[2]:  PA14:C   PA10:D
 // sercom2[3]:  PA15:C   PA11:D
 
-pub enum Sercom2Pad0 {
-    Pa8(pa8, PfD),
-    Pa12(pa12, PfC),
-}
+pad!(Sercom2Pad0 {
+    Pa8(PfD),
+    Pa12(PfC),
+});
 
-pub enum Sercom2Pad1 {
-    Pa9(pa9, PfD),
-    Pa13(pa13, PfC),
-}
+pad!(Sercom2Pad1 {
+    Pa9(PfD),
+    Pa13(PfC),
+});
 
-pub enum Sercom2Pad2 {
-    Pa10(pa10, PfD),
-    Pa14(pa14, PfC),
-}
+pad!(Sercom2Pad2 {
+    Pa10(PfD),
+    Pa14(PfC),
+});
 
-pub enum Sercom2Pad3 {
-    Pa11(pa11, PfD),
-    Pa15(pa15, PfC),
-}
+pad!(Sercom2Pad3 {
+    Pa11(PfD),
+    Pa15(PfC),
+});
 
 // sercom3[0]:  PA16:D   PA22:C
 // sercom3[1]:  PA17:D   PA23:C
 // sercom3[2]:  PA18:D   PA24:C   PA20:D
 // sercom3[3]:  PA19:D   PA25:C   PA21:D
 
-pub enum Sercom3Pad0 {
-    Pa16(pa16, PfD),
-    Pa22(pa22, PfC),
-}
-pub enum Sercom3Pad1 {
-    Pa17(pa17, PfD),
-    Pa23(pa23, PfC),
-}
-pub enum Sercom3Pad2 {
-    Pa18(pa18, PfD),
-    Pa20(pa20, PfD),
-    Pa24(pa24, PfC),
-}
-pub enum Sercom3Pad3 {
-    Pa19(pa19, PfD),
-    Pa21(pa21, PfD),
-    Pa25(pa25, PfC),
-}
-);
+pad!(Sercom3Pad0 {
+    Pa16(PfD),
+    Pa22(PfC),
+});
 
-pad!(
+pad!(Sercom3Pad1 {
+    Pa17(PfD),
+    Pa23(PfC),
+});
+
+pad!(Sercom3Pad2 {
+    Pa18(PfD),
+    Pa20(PfD),
+    Pa24(PfC),
+});
+
+pad!(Sercom3Pad3 {
+    Pa19(PfD),
+    Pa21(PfD),
+    Pa25(PfC),
+});
+
 // sercom4[0]:  PA12:D   PB08:D   PB12:C
 // sercom4[1]:  PA13:D   PB09:D   PB13:C
 // sercom4[2]:  PA14:D   PB10:D   PB14:C
 // sercom4[3]:  PA15:D   PB11:D   PB15:C
 
-pub enum Sercom4Pad0 {
-    Pa12(pa12, PfD),
-    Pb8(pb8, PfD),
-    Pb12(pb12, PfC),
-}
+pad!(Sercom4Pad0 {
+    Pa12(PfD),
+    Pb8(PfD),
+    Pb12(PfC),
+});
 
-pub enum Sercom4Pad1 {
-    Pa13(pa13, PfD),
-    Pb9(pb9, PfD),
-    Pb13(pb13, PfC),
-}
+pad!(Sercom4Pad1 {
+    Pa13(PfD),
+    Pb9(PfD),
+    Pb13(PfC),
+});
 
-pub enum Sercom4Pad2 {
-    Pa14(pa14, PfD),
-    Pb10(pb10, PfD),
-    Pb14(pb14, PfC),
-}
+pad!(Sercom4Pad2 {
+    Pa14(PfD),
+    Pb10(PfD),
+    Pb14(PfC),
+});
 
-pub enum Sercom4Pad3 {
-    Pa15(pa15, PfD),
-    Pb11(pb11, PfD),
-    Pb15(pb15, PfC),
-}
+pad!(Sercom4Pad3 {
+    Pa15(PfD),
+    Pb11(PfD),
+    Pb15(PfC),
+});
 
-pub enum Sercom5Pad2 {
-    Pa24(pa24, PfD),
-    Pb0(pb0, PfD),
-    Pa20(pa20, PfC),
-    Pb22(pb22, PfD),
-}
+// TODO confirm Sercom5 pad configurations
 
-pub enum Sercom5Pad3 {
-    Pa25(pa25, PfD),
-    Pb1(pb1, PfD),
-    Pa21(pa21, PfC),
-    Pb23(pb23, PfD),
-}
-);
+pad!(Sercom5Pad0 {
+    Pa23(PfD),
+    Pb2(PfD),
+    Pb31(PfD),
+    Pb16(PfC),
+});
 
-pad!(
-pub enum Sercom1Pad2 {
-    Pa18(pa18, PfC),
-    Pa30(pa30, PfD),
-    Pb22(pb22, PfC),
-}
+pad!(Sercom5Pad1 {
+    Pa22(PfD),
+    Pb3(PfD),
+    Pb30(PfD),
+    Pb17(PfC),
+});
 
-pub enum Sercom1Pad3 {
-    Pa19(pa19, PfC),
-    Pa31(pa31, PfD),
-    Pb23(pb23, PfC),
-}
+pad!(Sercom5Pad2 {
+    Pa24(PfD),
+    Pb0(PfD),
+    Pa20(PfC),
+    Pb22(PfD),
+});
 
-pub enum Sercom5Pad0 {
-    Pa23(pa23, PfD),
-    Pb2(pb2, PfD),
-    Pb31(pb31, PfD),
-    Pb16(pb16, PfC),
-}
-
-pub enum Sercom5Pad1 {
-    Pa22(pa22, PfD),
-    Pb3(pb3, PfD),
-    Pb30(pb30, PfD),
-    Pb17(pb17, PfC),
-}
-);
+pad!(Sercom5Pad3 {
+    Pa25(PfD),
+    Pb1(PfD),
+    Pa21(PfC),
+    Pb23(PfD),
+});
