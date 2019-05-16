61adde93c356fb100bffba1876dc75475136afc7
Keith Duncan <keith_duncan@me.com>
2019-05-16 10:30:55 +1000

    Add build-all.py again
    This parses the .travis.yml env to find boards to build.
    
    Using the travis file as a source of truth prevents duplication and having to update to lists of builds

---
+++ build-all.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import yaml
+import shlex
+import subprocess
+
+travis = yaml.safe_load(open('.travis.yml', 'r'))
+
+for env in travis['env']:
+	words = shlex.split(env)
+
+	d = {}
+	for word in words:
+		key_value = word.split('=', 1)
+		d[key_value[0]] = key_value[1]
+
+	crate_dir = d["CRATE"]
+
+	cmd = [
+	    'cargo',
+	    'build',
+	]
+
+	examples = d.get("EXAMPLES", None)
+	if examples != None:
+		cmd.extend(shlex.split(examples))
+
+	features = d.get("FEATURES", None)
+	if features != None:
+		cmd.extend(shlex.split(features))
+
+	print(cmd)
+	
+	subprocess.check_call(cmd, cwd=crate_dir)
