089497e1ad7f9462c8215e197125e448e964b13b
Paul Sajna <sajattack@gmail.com>
2019-08-30 16:55:39 -0700

    fix issue #88

--- pac/atsamd51j19a/src/tcc0/ccbuf_dith4.rs
+++ pac/atsamd51j19a/src/tcc0/ccbuf_dith4.rs
@@ -11,7 +11,7 @@ impl crate::ResetValue for super::CCBUF_DITH4 {
     }
 }
 #[doc = "Reader of field `CCBUF`"]
-pub type CCBUF_R = crate::R<u8, u8>;
+pub type CCBUF_R = crate::R<u32, u32>;
 #[doc = "Write proxy for field `CCBUF`"]
 pub struct CCBUF_W<'a> {
     w: &'a mut W,
@@ -19,13 +19,13 @@ pub struct CCBUF_W<'a> {
 impl<'a> CCBUF_W<'a> {
     #[doc = r"Writes raw bits to the field"]
     #[inline(always)]
-    pub unsafe fn bits(self, value: u8) -> &'a mut W {
-        self.w.bits = (self.w.bits & !0x0f) | ((value as u32) & 0x0f);
+    pub unsafe fn bits(self, value: u32) -> &'a mut W {
+        self.w.bits = (self.w.bits & !(0x000f_ffff << 4)) | (((value as u32) & 0x000f_ffff) << 4);
         self.w
     }
 }
 #[doc = "Reader of field `DITHERBUF`"]
-pub type DITHERBUF_R = crate::R<u32, u32>;
+pub type DITHERBUF_R = crate::R<u8, u8>;
 #[doc = "Write proxy for field `DITHERBUF`"]
 pub struct DITHERBUF_W<'a> {
     w: &'a mut W,
@@ -33,30 +33,30 @@ pub struct DITHERBUF_W<'a> {
 impl<'a> DITHERBUF_W<'a> {
     #[doc = r"Writes raw bits to the field"]
     #[inline(always)]
-    pub unsafe fn bits(self, value: u32) -> &'a mut W {
-        self.w.bits = (self.w.bits & !(0x000f_ffff << 4)) | (((value as u32) & 0x000f_ffff) << 4);
+    pub unsafe fn bits(self, value: u8) -> &'a mut W {
+        self.w.bits = (self.w.bits & !0x0f) | ((value as u32) & 0x0f);
         self.w
     }
 }
 impl R {
-    #[doc = "Bits 0:3 - Channel Compare/Capture Buffer Value"]
+    #[doc = "Bits 4:23 - Channel Compare/Capture Buffer Value"]
     #[inline(always)]
     pub fn ccbuf(&self) -> CCBUF_R {
-        CCBUF_R::new((self.bits & 0x0f) as u8)
+        CCBUF_R::new(((self.bits >> 4) & 0x000f_ffff) as u32)
     }
-    #[doc = "Bits 4:23 - Dithering Buffer Cycle Number"]
+    #[doc = "Bits 0:3 - Dithering Buffer Cycle Number"]
     #[inline(always)]
     pub fn ditherbuf(&self) -> DITHERBUF_R {
-        DITHERBUF_R::new(((self.bits >> 4) & 0x000f_ffff) as u32)
+        DITHERBUF_R::new((self.bits & 0x0f) as u8)
     }
 }
 impl W {
-    #[doc = "Bits 0:3 - Channel Compare/Capture Buffer Value"]
+    #[doc = "Bits 4:23 - Channel Compare/Capture Buffer Value"]
     #[inline(always)]
     pub fn ccbuf(&mut self) -> CCBUF_W {
         CCBUF_W { w: self }
     }
-    #[doc = "Bits 4:23 - Dithering Buffer Cycle Number"]
+    #[doc = "Bits 0:3 - Dithering Buffer Cycle Number"]
     #[inline(always)]
     pub fn ditherbuf(&mut self) -> DITHERBUF_W {
         DITHERBUF_W { w: self }
