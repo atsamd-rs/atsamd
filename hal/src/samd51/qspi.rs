3467b9657999815b8c79a192515f58370bea6ccc
Paul Sajna <sajattack@gmail.com>
2019-10-01 12:15:42 -0700

    low-level interface coming along

--- hal/src/samd51/qspi.rs
+++ hal/src/samd51/qspi.rs
@@ -1,6 +1,6 @@
 use crate::{
     target_device::{QSPI, MCLK}, 
-    gpio::{Pa8, Pa9, Pa10, Pa11, Pb10, Pb11, Input, Floating, PfH, Port}
+    gpio::{Pa8, Pa9, Pa10, Pa11, Pb10, Pb11, Input, Floating, PfH, Port},
 };
 
 pub struct Qspi {
@@ -64,4 +64,140 @@ impl Qspi {
             io3,
         }
     }
+    
+    unsafe fn run_instruction(
+        &mut self, 
+        command: Command, 
+        addr: u32, 
+        buf: &mut [u8]
+    ) {
+        if command == Command::EraseSector || command == Command::EraseBlock {
+            self.qspi.instraddr.write(|w| { w.addr().bits(addr) });
+        }
+        self.qspi.instrctrl.write(|w| w.instr().bits(command.bits()));
+        let _ = self.qspi.instrframe.read().bits();
+        if buf.len() > 0 {
+            if self.qspi.instrframe.read().tfrtype().is_read() 
+                || self.qspi.instrframe.read().tfrtype().is_readmemory()
+            {
+                core::ptr::copy((QSPI_AHB + addr) as *mut u8, buf.as_mut_ptr(), buf.len());  
+            } else {
+                core::ptr::copy(buf.as_ptr(), (QSPI_AHB + addr) as *mut u8, buf.len()); 
+            }
+        }
+
+        self.qspi.ctrla.write(|w| {
+            w.enable().set_bit();
+            w.lastxfer().set_bit()
+        });
+
+        while self.qspi.intflag.read().instrend().bit_is_clear() {}
+        self.qspi.intflag.modify(|_, w| w.instrend().set_bit());
+    }
+
+    pub fn run_command(&mut self, command: Command) {
+        self.qspi.instrframe.write(|w| {
+            w.width().single_bit_spi();
+            w.addrlen()._24bits();
+            w.tfrtype().read();
+            w.instren().set_bit()
+        });
+        unsafe { self.run_instruction(command, 0, &mut[]); }
+    }
+
+    pub fn read_command(&mut self, command: Command, response: &mut [u8]) {
+        self.qspi.instrframe.write(|w| {
+            w.width().single_bit_spi();
+            w.addrlen()._24bits();
+            w.tfrtype().read();
+            w.instren().set_bit();
+            w.dataen().set_bit()
+        });
+        unsafe { self.run_instruction(command, 0, response); }
+    }
+
+    pub fn write_command(&mut self, command: Command, data: &mut [u8]) {
+        self.qspi.instrframe.write(|w| {
+            w.width().single_bit_spi();
+            w.addrlen()._24bits();
+            w.tfrtype().write();
+            w.instren().set_bit();
+            if data.len() > 0 {
+                w.dataen().set_bit()
+            } else {
+                w.dataen().clear_bit()
+            }
+        });
+
+        unsafe { self.run_instruction(command, 0, data); }
+    }
+
+    pub fn erase_command(&mut self, command: Command, address: u32) {
+        self.qspi.instrframe.write(|w| {
+            w.width().single_bit_spi();
+            w.addrlen()._24bits();
+            w.tfrtype().write();
+            w.instren().set_bit();
+            w.addren().set_bit()
+        });
+        unsafe { self.run_instruction(command, address, &mut[]); }
+    }
+
+    pub fn read_memory(&mut self, addr: u32, buf: &mut [u8]) {
+        self.qspi.instrframe.write(|w| {
+            w.width().quad_output();
+            w.addrlen()._24bits();
+            w.tfrtype().read();
+            w.instren().set_bit();
+            w.dataen().set_bit();
+            w.addren().set_bit();
+            unsafe{ w.dummylen().bits(8) }
+        });
+        unsafe { self.run_instruction(Command::QuadRead, addr, buf) };
+    }
+
+    pub fn write_memory(&mut self, addr: u32, buf: &mut [u8]) {
+        self.qspi.instrframe.write(|w| {
+            w.width().quad_output();
+            w.addrlen()._24bits();
+            w.tfrtype().write();
+            w.instren().set_bit();
+            w.dataen().set_bit();
+            w.addren().set_bit()
+        });
+        unsafe { self.run_instruction(Command::QuadPageProgram, addr, buf) };
+    }
+
+    pub fn set_clk_divider(&mut self, value: u8) {
+        self.qspi.baud.write(|w| unsafe { w.baud().bits(value) });  
+    }
 }
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum Command {
+    Read = 0x03,
+    QuadRead = 0x6B,
+    ReadId = 0x9F,
+    PageProgram = 0x02,
+    QuadPageProgram = 0x32,
+    ReadStatus = 0x05,
+    ReadStatus2 = 0x35,
+    WriteStatus = 0x01,
+    WriteStatus2 = 0x31,
+    EnableReset = 0x66,
+    Reset = 0x99,
+    WriteEnable = 0x06,
+    WriteDisable = 0x04,
+    EraseSector = 0x20,
+    EraseBlock = 0xD8,
+    EraseChip = 0xC7
+}
+
+impl Command {
+    fn bits(self) -> u8 {
+        self as u8
+    }
+}
+
+const QSPI_AHB: u32 = 0x04000000;
