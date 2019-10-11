07eda4464460f9ab7ffba0441d39167f3e08c994
Twitch <twitchyliquid64@users.noreply.github.com>
2019-10-11 16:19:09 -0700

    samd51-hal: Implement watchdog, serial number, reset cause (#99)
    * Implement accessors for the reset cause and device serial number
    
    * Implement hal for watchdog
    
    * Bump crate minor versions

--- boards/itsybitsy_m4/examples/blinky_basic.rs
+++ boards/itsybitsy_m4/examples/blinky_basic.rs
@@ -9,6 +9,7 @@ use hal::delay::Delay;
 use hal::entry;
 use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
+use hal::watchdog::{Watchdog, WatchdogTimeout};
 
 #[entry]
 fn main() -> ! {
@@ -21,13 +22,19 @@ fn main() -> ! {
         &mut peripherals.OSCCTRL,
         &mut peripherals.NVMCTRL,
     );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+    delay.delay_ms(400u16);
+    let wdt = Watchdog::new_with_timeout(peripherals.WDT, WatchdogTimeout::Timeout256ms);
+
     let mut pins = hal::Pins::new(peripherals.PORT);
     let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
-    let mut delay = Delay::new(core.SYST, &mut clocks);
+
     loop {
         delay.delay_ms(200u8);
+        wdt.clear();
         red_led.set_high().unwrap();
         delay.delay_ms(200u8);
+        wdt.clear();
         red_led.set_low().unwrap();
     }
 }
