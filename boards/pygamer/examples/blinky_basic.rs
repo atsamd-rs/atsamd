0fad385371f33b6109378ce5ed06cec0e96812ca
Jacob <jacobrosenthal@gmail.com>
2019-10-19 14:49:48 -0700

    blinky

---
+++ boards/pygamer/examples/blinky_basic.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate pygamer as hal;
+
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::entry;
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
+use hal::watchdog::{Watchdog, WatchdogTimeout};
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+    delay.delay_ms(400u16);
+    let wdt = Watchdog::new_with_timeout(peripherals.WDT, WatchdogTimeout::Timeout256ms);
+
+    let mut pins = hal::Pins::new(peripherals.PORT);
+    let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
+
+    loop {
+        delay.delay_ms(200u8);
+        wdt.clear();
+        red_led.set_high().unwrap();
+        delay.delay_ms(200u8);
+        wdt.clear();
+        red_led.set_low().unwrap();
+    }
+}
