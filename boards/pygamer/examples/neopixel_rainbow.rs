d23f403f565515731bb1547937cfc414bcceedab
Jacob <jacobrosenthal@gmail.com>
2019-11-17 13:15:58 -0700

    nop still no good

--- boards/pygamer/examples/neopixel_rainbow.rs
+++ boards/pygamer/examples/neopixel_rainbow.rs
@@ -14,9 +14,8 @@ use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
 use hal::{clock::GenericClockController, delay::Delay};
 
-use smart_leds::brightness;
-use smart_leds::Color;
-use smart_leds::SmartLedsWrite;
+use smart_leds::hsv::RGB8;
+use smart_leds::{brightness, SmartLedsWrite};
 
 #[entry]
 fn main() -> ! {
@@ -36,18 +35,17 @@ fn main() -> ! {
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
     const NUM_LEDS: usize = 5;
-    let mut data = [Color::default(); NUM_LEDS];
+    let mut data = [RGB8::default(); NUM_LEDS];
 
     loop {
         for j in 0..(256 * 5) {
+            //why
             for _ in 0..1 {
                 for i in 0..NUM_LEDS {
                     data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
                 }
             }
-            neopixel
-                .write(brightness(data.iter().cloned(), 32))
-                .unwrap();
+            let _ = neopixel.write(brightness(data.iter().cloned(), 32));
             delay.delay_ms(5u8);
         }
     }
@@ -55,7 +53,7 @@ fn main() -> ! {
 
 /// Input a value 0 to 255 to get a color value
 /// The colours are a transition r - g - b - back to r.
-fn wheel(mut wheel_pos: u8) -> Color {
+fn wheel(mut wheel_pos: u8) -> RGB8 {
     wheel_pos = 255 - wheel_pos;
     if wheel_pos < 85 {
         return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
