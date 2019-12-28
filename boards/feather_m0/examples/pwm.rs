3ec2bf93453c09c4e230d44cbba37edcb9e45175
Jesse Braham <jesse@beta7.io>
2019-12-28 15:29:36 -0800

    Update PWM examples

--- boards/feather_m0/examples/pwm.rs
+++ boards/feather_m0/examples/pwm.rs
@@ -17,32 +17,31 @@ use cortex_m_rt::entry;
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
     let core = CorePeripherals::take().unwrap();
+
     let mut clocks = GenericClockController::with_external_32kosc(
         peripherals.GCLK,
         &mut peripherals.PM,
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
-
+    let mut delay = Delay::new(core.SYST, &mut clocks);
     let mut pins = hal::Pins::new(peripherals.PORT);
-    let d5 = pins.d5.into_function_e(&mut pins.port);
+
+    let _d5 = pins.d5.into_function_e(&mut pins.port);
+
     let gclk0 = clocks.gclk0();
     let mut pwm3 = Pwm3::new(
         &clocks.tcc2_tc3(&gclk0).unwrap(),
         1.khz(),
         peripherals.TC3,
-        hal::pwm::TC3Pinout::Pa15(d5),
         &mut peripherals.PM,
     );
-
     let max_duty = pwm3.get_max_duty();
 
-    let mut delay = Delay::new(core.SYST, &mut clocks);
-
     loop {
-        pwm3.set_duty(max_duty/2);
+        pwm3.set_duty(max_duty / 2);
         delay.delay_ms(1000u16);
-        pwm3.set_duty(max_duty/8);
+        pwm3.set_duty(max_duty / 8);
         delay.delay_ms(1000u16);
     }
 }
--- boards/trinket_m0/examples/pwm.rs
+++ boards/trinket_m0/examples/pwm.rs
@@ -2,14 +2,15 @@
 #![no_main]
 
 extern crate panic_halt;
-extern crate metro_m4 as hal;
+extern crate trinket_m0 as hal;
 extern crate cortex_m_rt;
 
 use hal::clock::GenericClockController;
 use hal::pac::{Peripherals, CorePeripherals};
 use hal::prelude::*;
 use hal::delay::Delay;
-use hal::pwm::Pwm2;
+use hal::pwm::{Channel, Pwm1};
+
 use cortex_m_rt::entry;
 
 #[entry]
@@ -17,31 +18,30 @@ fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
     let core = CorePeripherals::take().unwrap();
 
-    let mut clocks = GenericClockController::with_external_32kosc(
+    let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.GCLK,
-        &mut peripherals.MCLK,
-        &mut peripherals.OSC32KCTRL,
-        &mut peripherals.OSCCTRL,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
     let mut delay = Delay::new(core.SYST, &mut clocks);
     let mut pins = hal::Pins::new(peripherals.PORT);
 
-    let _d12 = pins.d12.into_function_e(&mut pins.port);
+    let _d13 = pins.d13.into_function_e(&mut pins.port);
 
     let gclk0 = clocks.gclk0();
-    let mut pwm2 = Pwm2::new(
-        &clocks.tc2_tc3(&gclk0).unwrap(),
+    let mut pwm1 = Pwm1::new(
+        &clocks.tcc0_tcc1(&gclk0).unwrap(),
         1.khz(),
-        peripherals.TC2,
-        &mut peripherals.MCLK,
+        peripherals.TCC1,
+        &mut peripherals.PM,
     );
-    let max_duty = pwm2.get_max_duty();
+    let max_duty = pwm1.get_max_duty();
 
     loop {
-        pwm2.set_duty(max_duty / 2);
+        pwm1.set_duty(Channel::_0, max_duty / 2);
         delay.delay_ms(1000u16);
-        pwm2.set_duty(max_duty / 8);
+        pwm1.set_duty(Channel::_0, max_duty / 8);
         delay.delay_ms(1000u16);
     }
 }
