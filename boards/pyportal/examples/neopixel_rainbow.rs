7fcb3c7881298e49af049a3201f96a22500fadb4
Jacob <jacobrosenthal@gmail.com>
2020-01-05 17:07:34 -0700

    pyportal: update smartleds and use spintimer

--- boards/pyportal/examples/neopixel_rainbow.rs
+++ boards/pyportal/examples/neopixel_rainbow.rs
@@ -5,13 +5,14 @@ extern crate cortex_m;
 extern crate panic_halt;
 extern crate pyportal as hal;
 extern crate smart_leds;
-extern crate ws2812_nop_samd51 as ws2812;
+extern crate ws2812_timer_delay as ws2812;
 
 use embedded_hal::digital::v1_compat::OldOutputPin;
 
 use hal::entry;
 use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
+use hal::timer::SpinTimer;
 use hal::{clock::GenericClockController, delay::Delay};
 
 use smart_leds::{
@@ -32,8 +33,9 @@ fn main() -> ! {
     );
     let mut pins = hal::Pins::new(peripherals.PORT);
 
+    let timer = SpinTimer::new(4);
     let neopixel_pin: OldOutputPin<_> = pins.neopixel.into_push_pull_output(&mut pins.port).into();
-    let mut neopixel = ws2812::Ws2812::new(neopixel_pin);
+    let mut neopixel = ws2812::Ws2812::new(timer, neopixel_pin);
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
     loop {
