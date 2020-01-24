e982734b10b683d838ed9ec6f1e6be61f0c28ea0
Jacob Rosenthal <jacobrosenthal@gmail.com>
2020-01-24 16:38:23 -0700

    Hsv pygamer (#157)
    * rustfmt
    
    * edgebadge: wasnt tested, fix blinky
    
    * replace wheel with hsv2rgb
    
    * edgebadge: make ws2812-timer a required dep like other bsps
    
    * pygamer: make ws2812-timer a required dep like other bsps
    
    * pyportal: update smartleds and use spintimer

--- boards/pyportal/examples/neopixel_rainbow.rs
+++ boards/pyportal/examples/neopixel_rainbow.rs
@@ -5,18 +5,20 @@ extern crate cortex_m;
 extern crate panic_halt;
 extern crate pyportal as hal;
 extern crate smart_leds;
-extern crate ws2812_nop_samd51 as ws2812;
+extern crate ws2812_timer_delay as ws2812;
 
-use embedded_hal::digital::v1_compat::{OldOutputPin};
+use embedded_hal::digital::v1_compat::OldOutputPin;
 
-use hal::prelude::*;
-use hal::{clock::GenericClockController, delay::Delay};
 use hal::entry;
 use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
+use hal::timer::SpinTimer;
+use hal::{clock::GenericClockController, delay::Delay};
 
-use smart_leds::brightness;
-use smart_leds::Color;
-use smart_leds::SmartLedsWrite;
+use smart_leds::{
+    hsv::{hsv2rgb, Hsv},
+    SmartLedsWrite,
+};
 
 #[entry]
 fn main() -> ! {
@@ -31,39 +33,20 @@ fn main() -> ! {
     );
     let mut pins = hal::Pins::new(peripherals.PORT);
 
+    let timer = SpinTimer::new(4);
     let neopixel_pin: OldOutputPin<_> = pins.neopixel.into_push_pull_output(&mut pins.port).into();
-    let mut neopixel = ws2812::Ws2812::new(neopixel_pin);
+    let mut neopixel = ws2812::Ws2812::new(timer, neopixel_pin);
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
-    const NUM_LEDS: usize = 1;
-    let mut data = [Color::default(); NUM_LEDS];
-
     loop {
-        for j in 0..(256 * 5) {
-            for _ in 0..1 {
-                for i in 0..NUM_LEDS {
-                    data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
-                }
-            }
-            neopixel
-                .write(brightness(data.iter().cloned(), 32))
-                .unwrap();
-            delay.delay_ms(1u8);
+        for j in 0..255u8 {
+            let colors = [hsv2rgb(Hsv {
+                hue: j,
+                sat: 255,
+                val: 32,
+            })];
+            neopixel.write(colors.iter().cloned()).unwrap();
+            delay.delay_ms(5u8);
         }
     }
-}
-
-/// Input a value 0 to 255 to get a color value
-/// The colours are a transition r - g - b - back to r.
-fn wheel(mut wheel_pos: u8) -> Color {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
-    }
-    if wheel_pos < 170 {
-        wheel_pos -= 85;
-        return (0, wheel_pos * 3, 255 - wheel_pos * 3).into();
-    }
-    wheel_pos -= 170;
-    (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
 }
