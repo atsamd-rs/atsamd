6ffb2edd67c66bb622d771064e44afd6c5d04231
Garret Kelly <gdk@google.com>
2020-05-14 08:49:44 -0400

    Switch from svdtools to XSLT
    Switch the SVD modifications from being made through svdtools' `svd
    patch` to using XSLT. Since SVD files are already XML files, XSLT is
    well-suited to modifying them.
    
    This change also includes fixes for the `ATSAMD11C14A` device which are
    not currently possible with `svdtools`.
    
    See #195.

--- update.sh
+++ update.sh
@@ -2,43 +2,30 @@
 
 set -xe
 
-
 # INSTALL DEPENDENCIES
 
 cargo install --force --git https://github.com/gkelly/svd2rust --branch \
     bleeding-edge --rev 2bbb60590096bcb67c91f38bedd1f63f98132abe svd2rust
 cargo install --force --version 0.7.0 form
 
-python -m pip install -r requirements.txt
+# PATCH SVD FILES AND GENERATE CRATES
 
+TOP="${PWD}"
 
-# PATCH SVD FILES
+for xsl in svd/devices/*\.xsl; do
+  chip=$(basename "${xsl}" .xsl)
+  CHIP=$(echo "${chip}" | tr '[:lower:]' '[:upper:]')
+  svd=svd/${CHIP}.svd
 
-count=$(find svd/ -maxdepth 1 -name "*.svd.patched" | wc -l)
-if [ "$count" != 0 ]; then 
-  rm svd/*.svd.patched
-fi 
+  pushd "${TOP}/pac/${chip}"
 
-for device in svd/devices/*.yaml; do
-  svd patch "$device"
-done
+  xsltproc "${TOP}/${xsl}" "${TOP}/${svd}" | svd2rust --nightly
 
-
-# GENERATE PAC FOR EACH DEVICE
-
-TOP=$PWD
-
-for svd in svd/*.svd.patched; do
-  CHIP=$(echo "${svd##*/}" | cut -f 1 -d '.')
-  chip=$(echo "$CHIP" | tr '[:upper:]' '[:lower:]')
-
-  cd "$TOP/pac/$chip"
   rm -rf src/
-
-  svd2rust --nightly -i "$TOP/$svd"
-  form -i lib.rs -o src/
+  form -i lib.rs -o src
   rm lib.rs
-
   cargo fmt
   rustfmt build.rs
+
+  popd
 done
