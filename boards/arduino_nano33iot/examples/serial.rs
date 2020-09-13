b89367fcdfc51f821a1780a540f3b60e58b0d330
Michal L <36729174+nebelgrau77@users.noreply.github.com>
2020-09-13 15:35:46 +0200

    Add serial.rs example
    Prints ASCII characters from ! to ~. Tested with FTDI-USB breakout board.

---
+++ boards/arduino_nano33iot/examples/serial.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+extern crate arduino_nano33iot as hal;
+
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::entry;
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut pins = hal::Pins::new(peripherals.PORT);    
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    let mut uart = hal::uart(&mut clocks,
+                         9600.hz(), 
+                         peripherals.SERCOM5,
+                         &mut peripherals.PM,
+                         pins.rx,
+                         pins.tx,
+                         &mut pins.port);
+                         
+    loop {                      
+        // print ASCII characters from ! to ~
+        for ch in 33..127 {
+            uart.write(ch).unwrap();
+            delay.delay_ms(500u16);
+            }        
+        }
+  }
