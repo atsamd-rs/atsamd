e44beb4811f30af98d8f46d3144a82cbd2b07cb5
Paul Sajna <sajattack@gmail.com>
2020-11-20 15:31:25 -0800

    Remove uart_debug

--- hal/src/common/thumbv7em/mod.rs
+++ hal/src/common/thumbv7em/mod.rs
@@ -24,22 +24,3 @@ pub mod pwm;
 
 #[cfg(feature = "unproven")]
 pub mod watchdog;
-
-#[cfg(feature = "use_uart_debug")]
-pub mod uart_debug;
-
-#[cfg(feature = "use_uart_debug")]
-#[macro_export]
-macro_rules! dbgprint {
-    ($($arg:tt)*) => {
-        {
-            use cortex_m::interrupt::free as disable_interrupts;
-            disable_interrupts(|_| unsafe {
-                {
-                    use core::fmt::Write;
-                    uart_debug::WRITER.write_fmt(format_args!($($arg)*)).unwrap();
-                };
-            });
-        }
-    };
-}
