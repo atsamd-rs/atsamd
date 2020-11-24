51d4e8393af54f7d6aca1f9acd658ba937e879ef
Tom <twitchyliquid64@ciphersink.net>
2020-11-24 15:40:52 -0800

    Implement step to update pac versions to latest in the HAL.

---
+++ .github/actions/sync-pac-versions/update-from-pac.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import os
+import re
+import sys
+import toml
+
+class PacDep(object):
+    def __init__(self, name, hal_info, pac_info):
+        self._name = name
+        self._hal_version = hal_info['version'][:3]
+        self._pac_version = pac_info['package']['version'][:3]
+        self._line = None
+
+    @property
+    def hal_version(self):
+        return self._hal_version
+
+    @property
+    def pac_version(self):
+        return self._pac_version
+
+    @property
+    def line(self):
+        return self._line
+
+    @line.setter
+    def line(self, value):
+        self._line = value
+
+# Populate a dictionary containing the different PACs and their version.
+pacs = dict()
+h = toml.load("hal/Cargo.toml")
+for dep, info in h['dependencies'].items():
+    is_pac = os.path.isdir("pac/" + dep)
+    if is_pac:
+        pacs[dep] = PacDep(dep, info, toml.load("pac/" + dep + "/Cargo.toml"))
+
+# Collect version and line information for each PAC.
+with open('hal/Cargo.toml', 'r') as f:
+    lines = f.read().splitlines()
+    for i, l in enumerate(lines):
+        l = l.strip()
+        if l.startswith('[dependencies.') and l.endswith(']'):
+            crate = re.search(r'(\[dependencies)\.(.+)]', l).group(2)
+            if crate in pacs:
+                pacs[crate].line = i
+
+# Update our line array
+for crate, p in pacs.items():
+    if p.hal_version != p.pac_version:
+        assert lines[p.line+2].startswith('version = "')
+        lines[p.line+2] = "version = \"%s\"" % p.pac_version
+        print("HAL dependency on %s upgraded from %s to %s." %
+                (crate, p.hal_version, p.pac_version),
+                file=sys.stderr)
+    else:
+        print("HAL dependency on %s is up-to-date." % crate, file=sys.stderr)
+
+with open('hal/Cargo.toml', 'w') as f:
+    f.write('\n'.join(lines) + '\n')
