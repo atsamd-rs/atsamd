aecbc70ae1a8aa00f5b6da0ba8e5fb013004afb8
Karsten Große <karsten.grosse@sympatron.de>
2020-11-27 22:41:31 +0100

    Made GMAC buffer descriptors use VolaileCell and added 'static bound to buffers and descriptors

--- hal/src/common/thumbv7em/gmac/buffer.rs
+++ hal/src/common/thumbv7em/gmac/buffer.rs
@@ -2,160 +2,161 @@
 
 use super::private;
 use generic_array::{ArrayLength, GenericArray};
+use vcell::VolatileCell;
 
 #[repr(C, align(8))]
 pub struct TxBufferDescriptor {
-    address: u32,
-    status: u32,
+    address: VolatileCell<u32>,
+    status: VolatileCell<u32>,
 }
 #[allow(clippy::len_without_is_empty)]
 impl TxBufferDescriptor {
     pub fn address(&self) -> u32 {
-        self.address
+        self.address.get()
     }
     pub(crate) fn set_address(&mut self, address: u32) {
-        self.address = address;
+        self.address.set(address);
     }
     pub(crate) fn reset_status(&mut self) {
-        self.status = 1 << 31; // only used bit
+        self.status.set(1 << 31); // only `used` bit
     }
     pub(crate) fn set_wrap(&mut self, wrap: bool) {
         if wrap {
-            self.address |= 1 << 30;
+            self.address.set(self.address.get() | (1 << 30));
         } else {
-            self.address &= !(1 << 30);
+            self.address.set(self.address.get() & !(1 << 30));
         }
     }
     pub(crate) fn reset_with_len(&mut self, len: usize, last_buf: bool) {
-        self.status = (len as u32) & ((1 << 14) - 1);
+        self.status.set((len as u32) & ((1 << 14) - 1));
         if last_buf {
-            self.status |= 1 << 16;
+            self.address.set(self.address.get() | (1 << 16));
         }
     }
     pub fn len(&self) -> u16 {
-        (self.status & ((1 << 14) - 1)) as u16
+        (self.status.get() & ((1 << 14) - 1)) as u16
     }
     pub fn last_buf(&self) -> bool {
-        (self.status >> 16) & 1 == 1
+        (self.status.get() >> 16) & 1 == 1
     }
     pub fn no_crc(&self) -> bool {
-        (self.status >> 17) & 1 == 1
+        (self.status.get() >> 17) & 1 == 1
     }
     pub fn checksum_err(&self) -> u8 {
-        (self.status >> 20 & 0x7) as u8
+        (self.status.get() >> 20 & 0x7) as u8
     }
     pub fn lco(&self) -> bool {
-        (self.status >> 26) & 1 == 1
+        (self.status.get() >> 26) & 1 == 1
     }
     pub fn exhausted(&self) -> bool {
-        (self.status >> 27) & 1 == 1
+        (self.status.get() >> 27) & 1 == 1
     }
     pub fn error(&self) -> bool {
-        (self.status >> 29) & 1 == 1
+        (self.status.get() >> 29) & 1 == 1
     }
     pub fn wrap(&self) -> bool {
-        (self.status >> 30) & 1 == 1
+        (self.status.get() >> 30) & 1 == 1
     }
     pub fn used(&self) -> bool {
-        (self.status >> 31) & 1 == 1
+        (self.status.get() >> 31) & 1 == 1
     }
 
     pub fn set_used(&mut self) {
-        self.status |= 1 << 31;
+        self.status.set(self.status.get() | (1 << 31));
     }
 }
 
 #[repr(C, align(8))]
 pub struct RxBufferDescriptor {
-    address: u32,
-    status: u32,
+    address: VolatileCell<u32>,
+    status: VolatileCell<u32>,
 }
 #[allow(clippy::len_without_is_empty)]
 impl RxBufferDescriptor {
     pub fn ownership(&self) -> bool {
-        self.address & 1 == 1
+        self.address.get() & 1 == 1
     }
     pub fn wrap(&self) -> bool {
-        (self.address >> 1) & 1 == 1
+        (self.address.get() >> 1) & 1 == 1
     }
     pub fn address(&self) -> u32 {
-        self.address >> 2
+        self.address.get() >> 2
     }
     pub(crate) fn set_address(&mut self, address: u32) {
         assert!(address < (1 << 30));
-        self.address = address & ((1 << 30) - 1);
+        self.address.set(address & ((1 << 30) - 1));
     }
     pub(crate) fn reset_status(&mut self) {
-        self.status = 0;
+        self.status.set(0);
     }
     pub(crate) fn set_wrap(&mut self, wrap: bool) {
         if wrap {
-            self.address |= 1 << 1;
+            self.address.set(self.address.get() | (1 << 1));
         } else {
-            self.address &= !(1 << 1);
+            self.address.set(self.address.get() & !(1 << 1));
         }
     }
 
     pub fn len(&self) -> u16 {
-        (self.status & ((1 << 13) - 1)) as u16
+        (self.status.get() & ((1 << 13) - 1)) as u16
     }
     pub fn fcs(&self) -> bool {
-        (self.status >> 13) & 1 == 1
+        (self.status.get() >> 13) & 1 == 1
     }
     pub fn sof(&self) -> bool {
-        (self.status >> 14) & 1 == 1
+        (self.status.get() >> 14) & 1 == 1
     }
     pub fn eof(&self) -> bool {
-        (self.status >> 15) & 1 == 1
+        (self.status.get() >> 15) & 1 == 1
     }
     pub fn cfi(&self) -> bool {
-        (self.status >> 16) & 1 == 1
+        (self.status.get() >> 16) & 1 == 1
     }
     pub fn vlan_priority(&self) -> u8 {
-        (self.status >> 17 & 0x7) as u8
+        (self.status.get() >> 17 & 0x7) as u8
     }
     pub fn priority_detected(&self) -> bool {
-        (self.status >> 20) & 1 == 1
+        (self.status.get() >> 20) & 1 == 1
     }
     pub fn vlan_detected(&self) -> bool {
-        (self.status >> 21) & 1 == 1
+        (self.status.get() >> 21) & 1 == 1
     }
     pub fn type_id_match(&self) -> u8 {
-        (self.status >> 22 & 0x3) as u8
+        (self.status.get() >> 22 & 0x3) as u8
     }
     pub fn checksumoffload(&self) -> bool {
-        (self.status >> 24) & 1 == 1
+        (self.status.get() >> 24) & 1 == 1
     }
     pub fn addrmatch(&self) -> u8 {
-        ((self.status >> 25) & 0x3) as u8
+        ((self.status.get() >> 25) & 0x3) as u8
     }
     pub fn ext_addr_match(&self) -> bool {
-        (self.status >> 27) & 1 == 1
+        (self.status.get() >> 27) & 1 == 1
     }
     pub fn uni_hash_match(&self) -> bool {
-        (self.status >> 29) & 1 == 1
+        (self.status.get() >> 29) & 1 == 1
     }
     pub fn multi_hash_match(&self) -> bool {
-        (self.status >> 30) & 1 == 1
+        (self.status.get() >> 30) & 1 == 1
     }
     pub fn boardcast_detect(&self) -> bool {
-        (self.status >> 31) & 1 == 1
+        (self.status.get() >> 31) & 1 == 1
     }
 }
 
 impl Default for RxBufferDescriptor {
     fn default() -> Self {
         RxBufferDescriptor {
-            address: 0,
-            status: 0,
+            address: VolatileCell::new(0),
+            status: VolatileCell::new(0),
         }
     }
 }
 impl Default for TxBufferDescriptor {
     fn default() -> Self {
         TxBufferDescriptor {
-            address: 0,
-            status: 0,
+            address: VolatileCell::new(0),
+            status: VolatileCell::new(0),
         }
     }
 }
@@ -209,12 +210,12 @@ impl<Size: ArrayLength<u8>> Default for GmacBuffer<Size> {
 
 pub struct GmacBufferSet<T, Count, Size>
 where
-    T: BufferDescriptor,
-    Count: ArrayLength<T> + ArrayLength<GmacBuffer<Size>>,
-    Size: ArrayLength<u8>,
+    T: 'static + BufferDescriptor,
+    Count: 'static + ArrayLength<T> + ArrayLength<GmacBuffer<Size>>,
+    Size: 'static + ArrayLength<u8>,
 {
-    descriptors: GenericArray<T, Count>,
-    buffers: GenericArray<GmacBuffer<Size>, Count>,
+    descriptors: &'static mut GenericArray<T, Count>,
+    buffers: &'static mut GenericArray<GmacBuffer<Size>, Count>,
 }
 
 impl<T, Count, Size> GmacBufferSet<T, Count, Size>
@@ -224,8 +225,8 @@ where
     Size: ArrayLength<u8>,
 {
     pub fn new(
-        mut descriptors: GenericArray<T, Count>,
-        buffers: GenericArray<GmacBuffer<Size>, Count>,
+        descriptors: &'static mut GenericArray<T, Count>,
+        buffers: &'static mut GenericArray<GmacBuffer<Size>, Count>,
     ) -> Self {
         let count = Count::to_usize();
         for (idx, (descriptor, buffer)) in descriptors.iter_mut().zip(buffers.iter()).enumerate() {
@@ -237,16 +238,16 @@ where
         }
     }
 
-    pub fn get_buffer(&self, index: usize) -> &[u8] {
+    pub(crate) fn get_buffer(&self, index: usize) -> &[u8] {
         &self.buffers[index][..]
     }
-    pub fn get_buffer_mut(&mut self, index: usize) -> &mut [u8] {
+    pub(crate) fn get_buffer_mut(&mut self, index: usize) -> &mut [u8] {
         &mut self.buffers[index][..]
     }
-    pub fn get_descriptor(&self, index: usize) -> &T {
+    pub(crate) fn get_descriptor(&self, index: usize) -> &T {
         &self.descriptors[index]
     }
-    pub fn get_descriptor_mut(&mut self, index: usize) -> &mut T {
+    pub(crate) fn get_descriptor_mut(&mut self, index: usize) -> &mut T {
         &mut self.descriptors[index]
     }
 }
@@ -286,11 +287,11 @@ pub trait BufferSet<T>: private::Sealed {
     fn get_descriptor_mut(&mut self, index: usize) -> &mut T;
 }
 
-impl<
-        T: BufferDescriptor,
-        Count: ArrayLength<T> + ArrayLength<GmacBuffer<Size>>,
-        Size: ArrayLength<u8>,
-    > BufferSet<T> for GmacBufferSet<T, Count, Size>
+impl<T, Count, Size> BufferSet<T> for GmacBufferSet<T, Count, Size>
+where
+    T: BufferDescriptor,
+    Count: ArrayLength<T> + ArrayLength<GmacBuffer<Size>>,
+    Size: ArrayLength<u8>,
 {
     const COUNT: usize = Count::USIZE;
     const SIZE: usize = Size::USIZE;
