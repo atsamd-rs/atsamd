1b81dfda7ce9171e85c1a363674bc1c8c04e9d6b
Tyler Holmes <tyler@holmesengineering.com>
2021-01-10 19:32:09 -0800

    fix USB & wifi clippy errors

--- hal/src/samd21/usb/bus.rs
+++ hal/src/samd21/usb/bus.rs
@@ -157,7 +157,7 @@ fn buffer() -> &'static mut [u8; BUFFER_SIZE] {
 }
 
 struct BufferAllocator {
-    buffers: &'static mut [u8; BUFFER_SIZE],
+    buffers: &'static mut [MaybeUninit<u8>; BUFFER_SIZE],
     next_buf: u16,
 }
 
@@ -172,7 +172,7 @@ impl BufferAllocator {
     fn allocate_buffer(&mut self, size: u16) -> UsbResult<*mut u8> {
         debug_assert!(size & 1 == 0);
 
-        let start_addr = &mut self.buffers[self.next_buf as usize] as *mut u8;
+        let start_addr = &mut self.buffers[self.next_buf as usize].as_mut_ptr();
         let buf_end = unsafe { start_addr.offset(BUFFER_SIZE as isize) };
 
         // The address must be 32-bit aligned, so allow for that here
