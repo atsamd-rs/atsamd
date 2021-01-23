502ea22ab31ab959a1e51c6d501ce504b8536127
Bradley Harden <bradleyharden@gmail.com>
2021-01-23 11:05:43 -0500

    Improve the implementation of AnyPin and AnyPad
    The AnyPin and AnyPad traits act as meta-types for the respective
    Pin<I, M> and Pad<S, P, M> types, and they encapsulate the Pin and Pad
    types with fewer type parameters.
    
    Although useful, it was difficult to convert between a generic type
    `P: AnyPin` and the corresponding `ConcretePin<P>`. Doing so required
    using custom `as_concrete` and related functions. To ease this burden,
    we repeated the entire `Pin` API on the `AnyPin` trait. However, that
    places an additional burden on the maintainer, because any changes to
    the API or documentation must be synchronized. Instead, it would be
    preferrable to simplify conversion between the abstract and concrete
    types, and then require users to perform that conversion manually.
    
    Create an `Is` trait that represents type identity. The super traits of
    `Is` enforce reflexive implementations of `From`, `AsRef` and `AsMut`
    for `Is::Type`, meaning `T: From<T> + AsRef<T> + AsMut<T>` is always
    true when `T: Is<Type = T>`. Implementations of `Is` must always have
    `Is::Type == Self`.
    
    Use the new `Is` trait to simplify converting between abstract and
    generic types. Change the names `ConcretePin` and `ConcretePad` to
    `SpecificPin` and `SpecificPad` to better match the names `AnyPin` and
    `AnyPad`. Apply the trait bound `AnyPin: Is<Type = SpecificPin<Self>>`
    and similarly for `AnyPad`. Doing so allows trivial conversion between
    `P: AnyPin` and `SpecificPin<P>` using `Into`, `AsRef` and `AsMut`.
    
    Update the documentation of the `pin` and `pads` modules to explain how
    to use `Into`, `AsRef` and `AsMut` for the conversion. Also make minor
    documentation updates in other areas as well.

--- hal/src/typelevel.rs
+++ hal/src/typelevel.rs
@@ -11,3 +11,35 @@ pub(crate) use private::Sealed;
 /// Type-level version of the [None](core::option::Option::None) variant
 pub struct NoneT;
 impl Sealed for NoneT {}
+
+/// Marker trait for type identity
+///
+/// This trait must be implemented with `Self::Type == Self`. When used as a
+/// trait bound with a constrained `Type`, i.e.
+/// `where T: Is<Type = SpecificType>`, it allows easy conversion between the
+/// generic type `T` and the `SpecificType` using [`Into`], [`AsRef`] and
+/// [`AsMut`].
+///
+/// This trait is used throughout the HAL to create various `Any*` meta-types.
+pub trait Is
+where
+    Self: From<IsType<Self>>,
+    Self: Into<IsType<Self>>,
+    Self: AsRef<IsType<Self>>,
+    Self: AsMut<IsType<Self>>,
+    IsType<Self>: AsRef<Self>,
+    IsType<Self>: AsMut<Self>,
+{
+    type Type;
+}
+
+/// Alias for [`Is`]`::Type`
+pub type IsType<T> = <T as Is>::Type;
+
+/// Blanket implementation
+impl<T> Is for T
+where
+    T: AsRef<T> + AsMut<T>,
+{
+    type Type = T;
+}
