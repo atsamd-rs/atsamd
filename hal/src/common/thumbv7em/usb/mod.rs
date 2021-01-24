fc51b6cdae8000b64ee58fb97fa73065430c0f86
ryan <ryan@kurte.nz>
2021-01-25 12:46:08 +1300

    Sealed USB traits

--- hal/src/common/thumbv7em/usb/mod.rs
+++ hal/src/common/thumbv7em/usb/mod.rs
@@ -2,6 +2,7 @@
 
 use crate::gpio;
 
+use private::UsbSealed;
 pub use usb_device;
 
 mod bus;
@@ -18,13 +19,13 @@ pub type DmPad = gpio::v1::Pa24<gpio::v1::PfH>;
 pub type DpPad = gpio::v1::Pa25<gpio::v1::PfH>;
 
 /// Indicates pin can be used as USB D-
-pub trait UsbPadDm: Send {}
+pub trait UsbPadDm: Send + private::UsbSealed {}
 
 /// Indicates pin can be used as USB D+
-pub trait UsbPadDp: Send {}
+pub trait UsbPadDp: Send + private::UsbSealed {}
 
 /// Indicates pin can be used as SOF (1kHz signal)
-pub trait UsbPadSof: Send {}
+pub trait UsbPadSof: Send + private::UsbSealed {}
 
 // v1 pin impls
 impl UsbPadSof for gpio::v1::Pa23<gpio::v1::PfH> {}
@@ -35,3 +36,19 @@ impl UsbPadDp for gpio::v1::Pa25<gpio::v1::PfH> {}
 impl UsbPadSof for gpio::v2::Pin<gpio::v2::PA23, gpio::v2::AlternateH> {}
 impl UsbPadDm for gpio::v2::Pin<gpio::v2::PA24, gpio::v2::AlternateH> {}
 impl UsbPadDp for gpio::v2::Pin<gpio::v2::PA25, gpio::v2::AlternateH> {}
+
+mod private {
+    use crate::gpio;
+
+    pub trait UsbSealed {}
+
+    // v1 pin impls
+    impl UsbSealed for gpio::v1::Pa23<gpio::v1::PfH> {}
+    impl UsbSealed for gpio::v1::Pa24<gpio::v1::PfH> {}
+    impl UsbSealed for gpio::v1::Pa25<gpio::v1::PfH> {}
+
+    // v2 pin impls
+    impl UsbSealed for gpio::v2::Pin<gpio::v2::PA23, gpio::v2::AlternateH> {}
+    impl UsbSealed for gpio::v2::Pin<gpio::v2::PA24, gpio::v2::AlternateH> {}
+    impl UsbSealed for gpio::v2::Pin<gpio::v2::PA25, gpio::v2::AlternateH> {}
+}
