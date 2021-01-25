e44aafd720e4ee2af7090441a86dfa147eb7d82e
Ryan <ryankurte@users.noreply.github.com>
2021-01-25 14:37:50 +1300

    Swap to USB pad trait for thumv7em targets (#365)
    * Introduce pad trait for USB GPIOs, backwards-compatible with v1
    
    * Sealed USB traits
    
    Co-authored-by: Twitch <twitchyliquid64@users.noreply.github.com>

--- hal/src/common/thumbv7em/usb/mod.rs
+++ hal/src/common/thumbv7em/usb/mod.rs
@@ -2,6 +2,7 @@
 
 use crate::gpio;
 
+use private::UsbSealed;
 pub use usb_device;
 
 mod bus;
@@ -10,11 +11,44 @@ pub use self::bus::UsbBus;
 mod devicedesc;
 use self::devicedesc::Descriptors;
 
-/// Emit SOF at 1Khz on this pin when configured as function H
-pub type SofPad = gpio::Pa23<gpio::PfH>;
+/// Default SOF pad
+pub type SofPad = gpio::v1::Pa23<gpio::v1::PfH>;
+/// Default USB D- pad
+pub type DmPad = gpio::v1::Pa24<gpio::v1::PfH>;
+/// Default USB D+ pad
+pub type DpPad = gpio::v1::Pa25<gpio::v1::PfH>;
 
-/// USB D- is connected here
-pub type DmPad = gpio::Pa24<gpio::PfH>;
+/// Indicates pin can be used as USB D-
+pub trait UsbPadDm: Send + private::UsbSealed {}
 
-/// USB D+ is connected here
-pub type DpPad = gpio::Pa25<gpio::PfH>;
+/// Indicates pin can be used as USB D+
+pub trait UsbPadDp: Send + private::UsbSealed {}
+
+/// Indicates pin can be used as SOF (1kHz signal)
+pub trait UsbPadSof: Send + private::UsbSealed {}
+
+// v1 pin impls
+impl UsbPadSof for gpio::v1::Pa23<gpio::v1::PfH> {}
+impl UsbPadDm for gpio::v1::Pa24<gpio::v1::PfH> {}
+impl UsbPadDp for gpio::v1::Pa25<gpio::v1::PfH> {}
+
+// v2 pin impls
+impl UsbPadSof for gpio::v2::Pin<gpio::v2::PA23, gpio::v2::AlternateH> {}
+impl UsbPadDm for gpio::v2::Pin<gpio::v2::PA24, gpio::v2::AlternateH> {}
+impl UsbPadDp for gpio::v2::Pin<gpio::v2::PA25, gpio::v2::AlternateH> {}
+
+mod private {
+    use crate::gpio;
+
+    pub trait UsbSealed {}
+
+    // v1 pin impls
+    impl UsbSealed for gpio::v1::Pa23<gpio::v1::PfH> {}
+    impl UsbSealed for gpio::v1::Pa24<gpio::v1::PfH> {}
+    impl UsbSealed for gpio::v1::Pa25<gpio::v1::PfH> {}
+
+    // v2 pin impls
+    impl UsbSealed for gpio::v2::Pin<gpio::v2::PA23, gpio::v2::AlternateH> {}
+    impl UsbSealed for gpio::v2::Pin<gpio::v2::PA24, gpio::v2::AlternateH> {}
+    impl UsbSealed for gpio::v2::Pin<gpio::v2::PA25, gpio::v2::AlternateH> {}
+}
