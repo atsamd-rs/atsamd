71c8db1916943e6b4e466d38fdbce34601ec7166
Erik Henriksson <erik.henriksson@me.com>
2021-02-02 23:25:03 +0100

    Make atsamd-hal build with no features specified (for library) and add a test (#394)
    Co-authored-by: Erik Henriksson <henriksson@google.com>

--- hal/src/common/mod.rs
+++ hal/src/common/mod.rs
@@ -1,9 +1,15 @@
+#[cfg(feature = "device")]
 pub mod delay;
+#[cfg(feature = "device")]
 pub mod gpio;
+#[cfg(feature = "device")]
 pub mod prelude;
+#[cfg(feature = "device")]
 pub mod rtc;
+#[cfg(feature = "device")]
 pub mod sercom;
 pub mod sleeping_delay;
+#[cfg(feature = "device")]
 pub mod spi_common;
 pub mod time;
 pub mod timer_params;
@@ -20,6 +26,7 @@ mod thumbv7em;
 pub use thumbv7em::*;
 
 // This module maintains backwards compatibility for the v1 SERCOM pads API
+#[cfg(feature = "device")]
 pub mod pad {
     pub use crate::sercom::v1::pads::PadPin;
 }
