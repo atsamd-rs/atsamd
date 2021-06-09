17a505c3cc775356bb964c507395aaea27f36273
DCRichards <DCRichards@live.co.uk>
2021-06-09 22:39:53 +0100

    cargo fmt

--- hal/src/thumbv6m/adc.rs
+++ hal/src/thumbv6m/adc.rs
@@ -98,16 +98,14 @@ impl Adc<ADC> {
 
     /// Set the prescaler for adjusting the clock relative to the system clock
     pub fn prescaler(&mut self, prescaler: Prescaler) {
-        self.adc.ctrlb.modify(|_, w| {
-            w.prescaler().variant(prescaler)
-        });
+        self.adc
+            .ctrlb
+            .modify(|_, w| w.prescaler().variant(prescaler));
     }
 
     /// Set the input resolution.
     pub fn resolution(&mut self, resolution: Resolution) {
-        self.adc.ctrlb.modify(|_, w| {
-            w.ressel().variant(resolution)
-        });
+        self.adc.ctrlb.modify(|_, w| w.ressel().variant(resolution));
     }
 
     fn power_up(&mut self) {
