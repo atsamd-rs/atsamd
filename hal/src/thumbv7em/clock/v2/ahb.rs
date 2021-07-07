6e1cedbb844e0bf65d6e5bd07ebccfafe7a5fecf
Gabriel Górski <gabriel.gorski@volvocars.com>
2021-07-07 19:27:25 +0200

    Ahb: Add documentation

--- hal/src/thumbv7em/clock/v2/ahb.rs
+++ hal/src/thumbv7em/clock/v2/ahb.rs
@@ -1,3 +1,27 @@
+#![deny(missing_docs)]
+#![deny(warnings)]
+//! # AHB bus clocks
+//!
+//! This module provides abstractions allowing to deal with a synchronous
+//! clocking domain, specifically modules clocked via AHB bus. It provides type
+//! representation for disabled and enabled synchronous clocks available through
+//! AHB bus and means of switching.
+//!
+//! - [`AhbToken<T>`] type represents a disabled clock for a peripheral of type
+//!   `T`: [`AhbType`]
+//! - [`AhbClk<T>`] type represents an enabled clock for a peripheral of type
+//!   `T:` [`AhbType`]
+//!
+//! One can enable a peripheral `T` synchronous clock via
+//! [`AhbToken<T>::enable`] `->` [`AhbClk<T>`] method.
+//!
+//! One can disable a peripheral `T` synchronous clock via
+//! [`AhbClk<T>::disable`] `->` [`AhbToken<T>`] method.
+//!
+//! Clocks in a default state are provided
+//! - in an instance of a struct [`AhbClks`]
+//! - in a field [`crate::clock::v2::Tokens::ahbs`]
+//! - in a return value of [`crate::clock::v2::retrieve_clocks`]
 use core::marker::PhantomData;
 
 use paste::paste;
@@ -8,18 +32,17 @@ use crate::pac::{mclk, MCLK};
 // Registers
 //==============================================================================
 
-/// TODO
+/// [`Registers`] struct is a low-level access abstraction for HW register
+/// calls. It is generic over [`AhbType`] as it needs appropriate mask values
+/// depending on a peripheral.
 struct Registers<A: AhbType> {
     ahb: PhantomData<A>,
 }
 
 impl<A: AhbType> Registers<A> {
-    /// TODO
     #[inline(always)]
     unsafe fn new() -> Self {
-        Registers {
-            ahb: PhantomData,
-        }
+        Registers { ahb: PhantomData }
     }
 
     #[inline(always)]
@@ -31,14 +54,13 @@ impl<A: AhbType> Registers<A> {
     fn ahbmask(&mut self) -> &mclk::AHBMASK {
         &self.mclk().ahbmask
     }
-    /// TODO
+
     #[inline(always)]
     fn enable(&mut self) {
         self.ahbmask()
             .modify(|r, w| unsafe { w.bits(r.bits() | A::MASK) });
     }
 
-    /// TODO
     #[inline(always)]
     fn disable(&mut self) {
         self.ahbmask()
@@ -50,13 +72,20 @@ impl<A: AhbType> Registers<A> {
 // AhbType
 //==============================================================================
 
-/// TODO
-pub trait AhbType {
+/// Trait implemented by a specific synchronous peripheral clock. Provides
+/// essential compile-time informations needed during HW register writes
+pub trait AhbType: crate::typelevel::Sealed {
+    /// Associated constant providing information regarding which mask offset
+    /// is applicable for this specific peripheral clock during HW register
+    /// writes
     const ID: AhbId;
+    /// Helper associated constant that expands to the specific mask from
+    /// a provided offset
     const MASK: u32 = 1 << (Self::ID as u8);
 }
 
-/// TODO
+/// Enum representing mask offsets for different peripheral clocks
+#[allow(missing_docs)]
 pub enum AhbId {
     Hpb0 = 0,
     Hpb1 = 1,
@@ -85,14 +114,16 @@ pub enum AhbId {
     NvmCtrlCache = 23,
 }
 
-/// TODO
+/// Macro implementing an [`AhbType`] for a marker type and defining one if
+/// necessary
 macro_rules! ahb_type {
-    // TODO
     (false, $Type:ident) => {
+        /// Marker type implementing [`AhbType`] for a specific synchronous peripheral
+        /// clock
         pub enum $Type {}
+        impl crate::typelevel::Sealed for $Type {}
         ahb_type!(true, $Type);
     };
-    // TODO
     (true, $Type:ident) => {
         impl AhbType for $Type {
             const ID: AhbId = AhbId::$Type;
@@ -104,13 +135,16 @@ macro_rules! ahb_type {
 // AhbToken
 //==============================================================================
 
-/// TODO
+/// A type representing a synchronous peripheral clock in a disabled state
 pub struct AhbToken<A: AhbType> {
     regs: Registers<A>,
 }
 
 impl<A: AhbType> AhbToken<A> {
-    /// TODO
+    /// Constructor
+    ///
+    /// Unsafe: There should always be only a single instance thereof. It is
+    /// being provided by a framework in a [`AhbClks`] struct instance
     #[inline]
     unsafe fn new() -> Self {
         AhbToken {
@@ -118,7 +152,7 @@ impl<A: AhbType> AhbToken<A> {
         }
     }
 
-    /// TODO
+    /// Enable a synchronous peripheral clock
     #[inline]
     pub fn enable(mut self) -> AhbClk<A> {
         self.regs.enable();
@@ -130,19 +164,19 @@ impl<A: AhbType> AhbToken<A> {
 // AhbClk
 //==============================================================================
 
-/// TODO
+/// A type representing a synchronous peripheral clock in an enabled state
 pub struct AhbClk<A: AhbType> {
     token: AhbToken<A>,
 }
 
 impl<A: AhbType> AhbClk<A> {
-    /// TODO
+    /// Constructor
     #[inline]
     fn new(token: AhbToken<A>) -> Self {
         AhbClk { token }
     }
 
-    /// TODO
+    /// Disable a synchronous peripheral clock
     #[inline]
     pub fn disable(mut self) -> AhbToken<A> {
         self.token.regs.disable();
@@ -154,7 +188,9 @@ impl<A: AhbType> AhbClk<A> {
 // AhbClks
 //==============================================================================
 
-/// TODO
+/// Root level macro generating all type definitions and trait implementations.
+/// Among other things, it defines and populates an [`AhbClks`] struct with
+/// appropriate fields.
 macro_rules! ahb_clks {
     (
         $(
@@ -167,7 +203,9 @@ macro_rules! ahb_clks {
                 $( #[$cfg] )?
                 ahb_type!($exists, $Type);
             )+
-            /// TODO
+            /// Struct aggregating [`AhbClk`] type instances representing
+            /// default states of synchronous peripheral clocks
+            #[allow(missing_docs)]
             pub struct AhbClks {
                 $(
                     $( #[$cfg] )?
@@ -175,7 +213,6 @@ macro_rules! ahb_clks {
                 )+
             }
             impl AhbClks {
-                /// TODO
                 pub(super) unsafe fn new() -> Self {
                     AhbClks {
                         $(
