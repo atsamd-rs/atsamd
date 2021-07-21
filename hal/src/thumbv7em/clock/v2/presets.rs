252d5a65edcda0d2fa76e2e72e39db0a681e465a
Gabriel Górski <gabriel.gorski@volvocars.com>
2021-07-21 16:54:30 +0200

    clock/v2/presets: Improve imports and doc-tests
    - Now, `clock::v2::presets` modules passes `cargo test` doc-test-wise
    - Explicit references to `atsamd_hal` crate instead of `crate`

--- hal/src/thumbv7em/clock/v2/presets.rs
+++ hal/src/thumbv7em/clock/v2/presets.rs
@@ -1,30 +1,20 @@
-#![allow(unused_imports)] // Imports are used in doc strings
-
-use super::dfll::*;
-use super::dpll::*;
-use super::gclk::*;
-use super::osculp32k::*;
-use super::pclk::*;
-use super::xosc32k::*;
-
-/// TODO: Make sure all these link are valid before integration. Task:
-/// Refine `Source` types and `SourceMarker` types and normalize naming.
-///
 /// This macro provides an opinionated clocking setup that consists of
+///
 /// a following chain of configured components:
 ///
-/// - [`Dfll<OpenLoop>`] (`48 MHz`)
-/// - [`Gclk5<Dfll>`] (`48 MHz / 24 -> 2 MHz`)
-/// - [`Pclk<Pll0, Gen5>`]
-/// - [`Dpll0<PclkDriven<Pll0, Gen5>>`] (`2 MHz * 60 -> 120 MHz`)
-/// - [`Gclk0<Pll0>`] (`120 MHz`)
+/// - [`Dfll<OpenLoop>`][`super::dfll::Dfll`] (`48 MHz`)
+/// - [`Gclk5<Dfll>`][`super::gclk::Gclk5`] (`48 MHz / 24 -> 2 MHz`)
+/// - [`Pclk<Pll0, Gen5>`][`super::pclk::Pclk`]
+/// - [`Dpll0<PclkDriven<Pll0, Gen5>>`][`super::dpll::Dpll0`] (`2 MHz * 60 -> 120
+///   MHz`)
+/// - [`Gclk0<Pll0>`][`super::gclk::Gclk0`] (`120 MHz`)
 ///
 /// Input arguments for this macro can be retrieved by calling
-/// [`crate::clock::v2::retrieve_clocks`] function.
+/// [`retrieve_clocks`][`crate::clock::v2::retrieve_clocks`] function.
 ///
 /// Example of usage:
 ///
-/// ```rust
+/// ```no_run
 /// let mut pac = atsamd_hal::pac::Peripherals::take().unwrap();
 ///
 /// let (gclk0, dfll, _, tokens) = atsamd_hal::clock::v2::retrieve_clocks(
@@ -44,6 +34,7 @@ macro_rules! clocking_preset_gclk0_120mhz_gclk5_2mhz {
         $dfll:expr,
         $tokens:expr
     ) => {{
+        use atsamd_hal::clock::v2::*;
         let (gclk5, dfll) = gclk::Gclk::new($tokens.gclks.gclk5, $dfll);
         let gclk5 = gclk5.div(gclk::GclkDiv::Div(24)).enable();
         let (pclk_dpll0, gclk5) = pclk::Pclk::enable($tokens.pclks.dpll0, gclk5);
@@ -54,31 +45,29 @@ macro_rules! clocking_preset_gclk0_120mhz_gclk5_2mhz {
         (gclk0, gclk5, dpll0, dfll)
     }};
 }
-/// TODO: Make sure all these link are valid before integration. Task:
-/// Refine `Source` types and `SourceMarker` types and normalize naming.
-///
+
 /// This macro provides an opinionated clocking setup that consists of
 /// a following chain of configured components:
 ///
-/// - [`Dfll<OpenLoop>`] (`48 MHz`)
-/// - [`Gclk5<Dfll>`] (`48 MHz / 24 -> 2 MHz`)
-/// - [`Pclk<Pll0, Gen5>`]
-/// - [`Dpll0<PclkDriven<Pll0, Gen5>>`] (`2 MHz * 60 -> 120 MHz`)
-/// - [`Gclk0<Pll0>`] (`120 MHz`)
+/// - [`Dfll<OpenLoop>`][`super::dfll::Dfll`] (`48 MHz`)
+/// - [`Gclk5<Dfll>`][`super::gclk::Gclk5`] (`48 MHz / 24 -> 2 MHz`)
+/// - [`Pclk<Pll0, Gen5>`][`super::pclk::Pclk`]
+/// - [`Dpll0<PclkDriven<Pll0, Gen5>>`][`super::dpll::Dpll0`] (`2 MHz * 60 -> 120 MHz`)
+/// - [`Gclk0<Pll0>`][`super::gclk::Gclk0`] (`120 MHz`)
 ///
 /// with configured external 32k crystal oscillator:
 ///
-/// - [`Xosc32k<Xosc32kMode>`] (`32 KHz`)
-/// - [`Gclk1<Osc32k>`] (`32 KHz`)
+/// - [`Xosc32k<Xosc32kMode>`][`super::xosc32k::Xosc32k`] (`32 KHz`)
+/// - [`Gclk1<Osc32k>`][`super::gclk::Gclk1`] (`32 KHz`)
 ///
 /// Input arguments for this macro can be retrieved by calling
-/// [`crate::clock::v2::retrieve_clocks`] function.
+/// [`retrieve_clocks`][`crate::clock::v2::retrieve_clocks`] function.
 ///
 /// Example of usage:
 ///
-/// ```rust
+/// ```no_run
 /// let mut pac = atsamd_hal::pac::Peripherals::take().unwrap();
-/// let pins = atsamd_hal::gpio::v2::Pins::new(peripherals.PORT);
+/// let pins = atsamd_hal::gpio::v2::Pins::new(pac.PORT);
 ///
 /// let (gclk0, dfll, _, tokens) = atsamd_hal::clock::v2::retrieve_clocks(
 ///     pac.OSCCTRL,
@@ -101,39 +90,38 @@ macro_rules! clocking_preset_gclk0_120mhz_gclk5_2mhz_gclk1_external_32khz {
         $xosc32k_out:expr,
         $tokens:expr
     ) => {{
+        use atsamd_hal::clock::v2::*;
         let (gclk0, gclk5, dpll0, dfll) =
-            crate::clocking_preset_gclk0_120mhz_gclk5_2mhz!($gclk0, $dfll, $tokens);
+            atsamd_hal::clocking_preset_gclk0_120mhz_gclk5_2mhz!($gclk0, $dfll, $tokens);
         let xosc32k = xosc32k::Xosc32k::from_crystal($tokens.xosc32k, $xosc32k_in, $xosc32k_out)
-            .enable_32k(true)
-            .enable_1k(true)
-            .enable();
+            .enable()
+            .activate_32k()
+            .activate_1k();
         let (gclk1, xosc32k) = gclk::Gclk::new($tokens.gclks.gclk1, xosc32k);
         (gclk0, gclk5, gclk1, xosc32k, dpll0, dfll)
     }};
 }
-/// TODO: Make sure all these link are valid before integration. Task:
-/// Refine `Source` types and `SourceMarker` types and normalize naming.
-///
+
 /// This macro provides an opinionated clocking setup that consists of
 /// a following chain of configured components:
 ///
-/// - [`Dfll<OpenLoop>`] (`48 MHz`)
-/// - [`Gclk5<Dfll>`] (`48 MHz / 24 -> 2 MHz`)
-/// - [`Pclk<Pll0, Gen5>`]
-/// - [`Dpll0<PclkDriven<Pll0, Gen5>>`] (`2 MHz * 60 -> 120 MHz`)
-/// - [`Gclk0<Pll0>`] (`120 MHz`)
+/// - [`Dfll<OpenLoop>`][`super::dfll::Dfll`] (`48 MHz`)
+/// - [`Gclk5<Dfll>`][`super::gclk::Gclk5`] (`48 MHz / 24 -> 2 MHz`)
+/// - [`Pclk<Pll0, Gen5>`][`super::pclk::Pclk`]
+/// - [`Dpll0<PclkDriven<Pll0, Gen5>>`][`super::dpll::Dpll0`] (`2 MHz * 60 -> 120 MHz`)
+/// - [`Gclk0<Pll0>`][`super::gclk::Gclk0`] (`120 MHz`)
 ///
 /// with internal, ultra low power 32k oscillator:
 ///
-/// - [`OscUlp32k`] (`32 KHz`)
-/// - [`Gclk1<Ulp32k>`] (`32 KHz`)
+/// - [`OscUlp32k`][`super::osculp32k::OscUlp32k`] (`32 KHz`)
+/// - [`Gclk1<Ulp32k>`][`super::gclk::Gclk1`] (`32 KHz`)
 ///
 /// Input arguments for this macro can be retrieved by calling
-/// [`crate::clock::v2::retrieve_clocks`] function.
+/// [`retrieve_clocks`][`crate::clock::v2::retrieve_clocks`] function.
 ///
 /// Example of usage:
 ///
-/// ```rust
+/// ```no_run
 /// let mut pac = atsamd_hal::pac::Peripherals::take().unwrap();
 ///
 /// let (gclk0, dfll, osculp32k, tokens) = atsamd_hal::clock::v2::retrieve_clocks(
@@ -156,8 +144,9 @@ macro_rules! clocking_preset_gclk0_120mhz_gclk5_2mhz_gclk1_internal_32khz {
         $osculp32k:expr,
         $tokens:expr
     ) => {{
+        use atsamd_hal::clock::v2::*;
         let (gclk0, gclk5, dpll0, dfll) =
-            crate::clocking_preset_gclk0_120mhz_gclk5_2mhz!($gclk0, $dfll, $tokens);
+            atsamd_hal::clocking_preset_gclk0_120mhz_gclk5_2mhz!($gclk0, $dfll, $tokens);
         let (gclk1, osculp32k) = gclk::Gclk::new($tokens.gclks.gclk1, $osculp32k);
         (gclk0, gclk5, gclk1, osculp32k, dpll0, dfll)
     }};
