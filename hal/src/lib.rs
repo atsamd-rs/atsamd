905eb13c702a7a8f292bd8c1f0e395d4e5ee7d5b
Ian Rees <code@ianrees.nz>
2021-07-30 20:45:17 +1200

    Add `library` feature, reword errors and comments

--- hal/src/lib.rs
+++ hal/src/lib.rs
@@ -6,8 +6,12 @@ pub use paste;
 
 pub mod typelevel;
 
-#[cfg(not(feature = "device"))]
-compile_error!("The HAL requires a target device selection via a Cargo feature");
+#[cfg(not(any(feature = "library", feature = "device")))]
+compile_error!(
+    "The HAL is usually built for a specific target device, selected using a \
+    feature.  If atsamd-hal is being built as a library, bypass this check by \
+    specifying the `library` feature"
+);
 
 #[cfg(feature = "samd11c")]
 pub use atsamd11c as target_device;
@@ -101,9 +105,14 @@ pub mod timer_traits;
 #[cfg(all(feature = "unproven", feature = "dma"))]
 pub mod dmac;
 
-// The SAMD11 hardware supports USB too, but the SAMD11 HAL currently doesn't
-#[cfg(all(feature = "usb", not(feature = "samd21"), not(feature = "min-samd51g")))]
-compile_error!("The 'usb' feature is enabled, but not a specific chip that supports USB");
+#[cfg(all(feature = "usb", feature = "samd11"))]
+compile_error!("'usb' is enabled, but USB isn't supported on SAMD11");
+
+#[cfg(all(
+    feature = "usb",
+    not(any(feature = "samd21", feature = "min-samd51g", feature = "library"))
+))]
+compile_error!("The 'usb' feature is enabled, but not a chip with USB support");
 
 #[cfg(any(feature = "samd11", feature = "samd21"))]
 pub mod thumbv6m;
