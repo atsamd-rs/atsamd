24d0ea42e35cc4861e91a2c1473642fbe31ad1f9
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-09-14 20:44:14 -0700

    eic fix unused borrow that must be used

--- hal/src/thumbv7em/eic/pin.rs
+++ hal/src/thumbv7em/eic/pin.rs
@@ -100,7 +100,7 @@ crate::paste::item! {
 
         pub fn clear_interrupt(&mut self) {
             unsafe {
-                &(*target_device::EIC::ptr()).intflag.write(|w| {
+                (*target_device::EIC::ptr()).intflag.write(|w| {
                     w.bits(1 << $num)
                 });
             }
