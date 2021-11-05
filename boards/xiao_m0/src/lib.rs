335b16e1d2fb53a984ffde2a51a1d922a05db790
avan1235 <macieekprocyk@gmail.com>
2021-11-05 22:44:36 +0100

    PR notes fixes: - bring back comments with pins names - fix CHANGELOG.md

--- boards/xiao_m0/src/lib.rs
+++ boards/xiao_m0/src/lib.rs
@@ -25,84 +25,100 @@ pub mod pins {
 
     hal::bsp_pins!(
         PA02 {
+            /// Pin A0/D0/DAC
             name: a0
         }
         PA04 {
+            /// Pin A1/D1
             name: a1
         }
         PA10 {
+            /// Pin A2/D2
             name: a2
         }
         PA11 {
+            /// Pin A3/D3
             name: a3
         }
         PA08 {
+            /// Pin A4/D4/SDA
             name: a4
             aliases: {
                 AlternateC: Sda
             }
         }
         PA09 {
+            /// Pin A5/D5/SCL
             name: a5
             aliases: {
                 AlternateC: Scl
             }
         }
         PB08 {
+            /// Pin A6/D6/TX
             name: a6
             aliases: {
                 AlternateD: UartTx
             }
         }
         PB09 {
+            /// Pin A7/D7/RX
             name: a7
             aliases: {
                 AlternateD: UartRx
             }
         }
         PA07 {
+            /// Pin A8/D8/SCK
             name: a8
             aliases: {
                 AlternateD: Sclk
             }
         }
         PA05 {
+            /// Pin A9/D9/MISO
             name: a9
             aliases: {
                 AlternateD: Miso
             }
         }
         PA06 {
+            /// Pin A10/D10/MOSI
             name: a10
             aliases: {
                 AlternateD: Mosi
             }
         }
         PA17 {
+            /// On-board yellow 'L' LED
             name: led0
             aliases: {
                 PushPullOutput: Led0
             }
         }
         PA18 {
+            /// On-board blue 'RX' LED
             name: led1
             aliases: {
                 PushPullOutput: Led1
             }
         }
         PA19 {
+            /// On-board blue 'TX' LED
             name: led2
             aliases: {
                 PushPullOutput: Led2
             }
         }
         PA24 {
+            /// The USB D- pad
             name: usb_dm
             aliases: {
                 AlternateG: UsbDm
             }
         }
         PA25 {
+            /// The USB D+ pad
             name: usb_dp
             aliases: {
                 AlternateG: UsbDp
