ce53a34abea4fe0c3ccaab2fcf617b631ca1f576
Tyler Holmes <tyler@holmesengineering.com>
2022-01-07 19:42:35 -0800

    make the build-all arg names better

--- build-all.py
+++ build-all.py
@@ -6,7 +6,7 @@ import subprocess
 from pathlib import PurePath
 
 
-def main(clean=False, just_clean=False):
+def main(clean=False, build=False):
     with open("crates.json", "r") as f:
         crates = json.load(f)
 
@@ -19,10 +19,10 @@ def main(clean=False, just_clean=False):
         command = shlex.split(jobs["build"])
         crate_path = PurePath("boards", board)
 
-        if clean or just_clean:
+        if clean:
             subprocess.check_call(shlex.split("cargo clean"), cwd=crate_path)
             subprocess.check_call(shlex.split("cargo update"), cwd=crate_path)
-        if not just_clean:
+        if build:
             subprocess.check_call(command, cwd=crate_path)
 
 
@@ -30,9 +30,9 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--clean", action="store_true", help="clean the crate before building")
-    parser.add_argument("--just-clean", action="store_true", help="Only clean the crate and delete Cargo.lock")
+    parser.add_argument("--clean", action="store_true", help="run `cargo clean` and `cargo update` before building")
+    parser.add_argument("--no-build", action="store_true", help="Don't build the crates")
 
     args = parser.parse_args()
 
-    main(clean=args.clean, just_clean=args.just_clean)
+    main(clean=args.clean, build=not args.no_build)
