c293f33203e80b85a26574d03f17046f8fb368b0
Ian McFarland <ian@neo.com>
2023-01-29 19:20:24 -0800

    Made the Xiao blink with the milspec preferred blink style of 3 on, 1 off.

---
+++ boards/xiao_m0/examples/milspec_blink.rs
@@ -0,0 +1,34 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_halt;
+
+use hal::{clock::GenericClockController, delay::Delay, prelude::*};
+use pac::{CorePeripherals, Peripherals};
+
+use bsp::{entry, hal, pac, Led0};
+use xiao_m0 as bsp;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let pins = bsp::Pins::new(peripherals.PORT);
+
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+    let mut led0: Led0 = pins.led0.into_push_pull_output();
+
+    loop {
+        delay.delay_ms(100u8);
+        led0.toggle().unwrap();
+        delay.delay_ms(200u8);
+        delay.delay_ms(100u8);
+        led0.toggle().unwrap();
+    }
+}
