df032cbfdd1e67168ceec5aedaf3352686275308
Felix Lelchuk <felix-el@users.noreply.github.com>
2023-04-26 23:21:22 +0200

    Add `feather_m4_can` board, add `mcan` example
    - the new `feather_m4_can` board is added as a clone
      of `feather_m4` with the chip changed ATSAM(D->E)51J
    
    - apply changes in board layout from `Feather M4`
      - adapt pinmux: alternate function pins for CAN1 connecting
        to TCAN1051 transceiver (RX, TX, S) and BOOST_EN
      - add aref pin on PA03
      - move battery pin from PB01 to PB00
      - Neopixel: update data to be on PB02, add enable pin PB03
    
    - add neopixel rainbow example
    - adapt `mcan` example from `xplain` board replacing
      button triggering by a periodic task

---
+++ boards/feather_m4_can/examples/timers.rs
@@ -0,0 +1,51 @@
+#![no_std]
+#![no_main]
+
+// Simple Peripheral Timer Example
+
+use bsp::hal;
+use feather_m4_can as bsp;
+
+#[cfg(not(feature = "use_semihosting"))]
+use panic_halt as _;
+#[cfg(feature = "use_semihosting")]
+use panic_semihosting as _;
+
+use bsp::entry;
+use hal::clock::GenericClockController;
+use hal::pac::Peripherals;
+use hal::prelude::*;
+
+use hal::timer::TimerCounter;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    // Using the red LED as the feedback for this simple timer example.
+    let pins = bsp::Pins::new(peripherals.PORT);
+    let mut red_led = pins.d13.into_push_pull_output();
+
+    // gclk0 represents a configured clock using the 120MHz oscillator.
+    let gclk0 = clocks.gclk0();
+    // Configure a clock for TC2 and TC3 peripherals
+    let timer_clock = clocks.tc2_tc3(&gclk0).unwrap();
+    //Instantiate a timer object for the TC2 peripheral
+    let mut timer = TimerCounter::tc2_(&timer_clock, peripherals.TC2, &mut peripherals.MCLK);
+    // Start the timer such that it runs at 50Hz
+    timer.start(50u32.hz());
+
+    // Toggle the red LED at the frequency set by the timer above.
+    loop {
+        red_led.set_high().unwrap();
+        nb::block!(timer.wait()).ok();
+        red_led.set_low().unwrap();
+        nb::block!(timer.wait()).ok();
+    }
+}
