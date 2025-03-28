164b72241819340c05e43215629153f55065c7e5
Ellie Frost <web@stillinbeta.com>
2025-03-27 18:52:47 -0700

    Require a token of rtc configuration

--- boards/pyportal/examples/embassy_timer.rs
+++ boards/pyportal/examples/embassy_timer.rs
@@ -7,7 +7,8 @@ use panic_halt as _;
 use panic_semihosting as _;
 
 use bsp::{hal, pac, pin_alias};
-use hal::ehal::digital::StatefulOutputPin;
+use hal::{ehal::digital::StatefulOutputPin,
+          clock::v2::{rtcosc::RtcOsc, osculp32k::OscUlp32k, clock_system_at_reset}};
 use pyportal as bsp;
 
 use embassy_time::Timer;
@@ -16,15 +17,28 @@ hal::embassy_time!(Driver);
 
 #[embassy_executor::main]
 async fn main(_s: embassy_executor::Spawner) {
-    let peripherals = pac::Peripherals::take().unwrap();
+    let mut peripherals = pac::Peripherals::take().unwrap();
     let _core = pac::CorePeripherals::take().unwrap();
     let pins = bsp::Pins::new(peripherals.port);
     let mut red_led: bsp::RedLed = pin_alias!(pins.red_led).into();
 
     // Select the 32khz source
+    let (_, clocks, tokens) = clock_system_at_reset(
+        peripherals.oscctrl,
+        peripherals.osc32kctrl,
+        peripherals.gclk,
+        peripherals.mclk,
+        &mut peripherals.nvmctrl,
+    );
+
+    let (osculp32k, _) = OscUlp32k::enable(tokens.osculp32k.osculp32k, clocks.osculp32k_base);
+
+    let (rtc, _) = RtcOsc::enable(tokens.rtcosc, osculp32k);
+
     // SAFETY: not in a critical section
+
     unsafe {
-        Driver::init();
+        Driver::init(rtc);
     }
 
     loop {
