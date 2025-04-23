ef3cad58fede500fdf182a464b5393623fd64d64
Pawel Pyszko <pyszkomeister@gmail.com>
2025-04-23 14:43:47 +0000

    Provide a stub for SPI interface for display

--- boards/wio_terminal/src/display.rs
+++ boards/wio_terminal/src/display.rs
@@ -1,13 +1,17 @@
 use atsamd_hal::clock::GenericClockController;
+use atsamd_hal::ehal;
 use atsamd_hal::ehal::delay::DelayNs;
+use atsamd_hal::ehal_nb::serial::Write;
 use atsamd_hal::ehal::digital::OutputPin;
-use atsamd_hal::ehal::spi::{Phase, Polarity};
+use atsamd_hal::ehal::spi::{Phase, Polarity, SpiBus, SpiDevice};
 use atsamd_hal::pac::{MCLK, SERCOM7};
 use atsamd_hal::sercom::spi;
+use atsamd_hal::sercom::spi::Spi;
 use atsamd_hal::sercom::{IoSet4, Sercom7};
 use atsamd_hal::time::Hertz;
 use atsamd_hal::typelevel::NoneT;
 use display_interface_spi::SPIInterface;
+//  use display_interface::WriteOnlyDataCommand;
 use ili9341::{DisplaySize240x320, Ili9341, Orientation};
 
 use super::pins::aliases::*;
@@ -38,11 +42,94 @@ pub struct Display {
 
 pub type LcdPads = spi::Pads<Sercom7, IoSet4, NoneT, LcdMosi, LcdSck>;
 pub type LcdSpi = spi::Spi<spi::Config<LcdPads>, spi::Tx>;
+pub type LcdSpiWrapper = SpiDeviceWrapper<LcdCs>;
 
 /// Type alias for the ILI9341 LCD display.
-pub type LCD = Ili9341<SPIInterface<LcdSpi, LcdDc, LcdCs>, LcdReset>;
+//  pub type LCD = Ili9341<SPIInterface<LcdSpi, LcdDc>, LcdReset>;
+pub type LCD = Ili9341<SPIInterface<LcdSpiWrapper, LcdDc>, LcdReset>;
+//  impl<SPI, DC> WriteOnlyDataCommand for SPIInterface<SPI, DC>
+//  where
+//      SPI: SpiDevice,
+//      DC: OutputPin,
 
 pub use ili9341::Scroller;
+
+struct SpiDeviceWrapper<CS>{
+    spi: LcdSpi,
+    dc: CS,
+}
+
+impl<CS> SpiDeviceWrapper<CS> {
+    fn new(spi: LcdSpi, dc: CS) -> Self {
+        Self { spi, dc }
+    }
+}
+use atsamd_hal::ehal::spi::{Operation, ErrorType as SpiErrorType};
+impl<Cs> SpiErrorType for SpiDeviceWrapper<Cs> {
+    type Error = spi::Error;
+}
+impl<Cs, Word: Copy + 'static> ehal::spi::SpiDevice<Word> for SpiDeviceWrapper<Cs> {
+    /// Perform a transaction against the device.
+    ///
+    /// - Locks the bus
+    /// - Asserts the CS (Chip Select) pin.
+    /// - Performs all the operations.
+    /// - [Flushes](SpiBus::flush) the bus.
+    /// - Deasserts the CS pin.
+    /// - Unlocks the bus.
+    ///
+    /// The locking mechanism is implementation-defined. The only requirement is it must prevent two
+    /// transactions from executing concurrently against the same bus. Examples of implementations are:
+    /// critical sections, blocking mutexes, returning an error or panicking if the bus is already busy.
+    ///
+    /// On bus errors the implementation should try to deassert CS.
+    /// If an error occurs while deasserting CS the bus error should take priority as the return value.
+    fn transaction(&mut self, operations: &mut [Operation<'_, Word>]) -> Result<(), Self::Error>{
+        todo!()
+    }
+
+    /// Do a read within a transaction.
+    ///
+    /// This is a convenience method equivalent to `device.transaction(&mut [Operation::Read(buf)])`.
+    ///
+    /// See also: [`SpiDevice::transaction`], [`SpiBus::read`]
+    #[inline]
+    fn read(&mut self, buf: &mut [Word]) -> Result<(), Self::Error> {
+       // self.spi.read(buf)
+       todo!() 
+    }
+
+    /// Do a write within a transaction.
+    ///
+    /// This is a convenience method equivalent to `device.transaction(&mut [Operation::Write(buf)])`.
+    ///
+    /// See also: [`SpiDevice::transaction`], [`SpiBus::write`]
+    #[inline]
+    fn write(&mut self, buf: &[Word]) -> Result<(), Self::Error> {
+       //  self.spi.write(buf)
+       todo!()
+    }
+
+    /// Do a transfer within a transaction.
+    ///
+    /// This is a convenience method equivalent to `device.transaction(&mut [Operation::Transfer(read, write)]`.
+    ///
+    /// See also: [`SpiDevice::transaction`], [`SpiBus::transfer`]
+    #[inline]
+    fn transfer(&mut self, read: &mut [Word], write: &[Word]) -> Result<(), Self::Error> {
+        todo!()
+    }
+
+    /// Do an in-place transfer within a transaction.
+    ///
+    /// This is a convenience method equivalent to `device.transaction(&mut [Operation::TransferInPlace(buf)]`.
+    ///
+    /// See also: [`SpiDevice::transaction`], [`SpiBus::transfer_in_place`]
+    #[inline]
+    fn transfer_in_place(&mut self, buf: &mut [Word]) -> Result<(), Self::Error> {
+        todo!()
+    }
+}
 
 impl Display {
     /// Initialize the display and its corresponding SPI bus peripheral. Return
@@ -70,9 +157,11 @@ impl Display {
         let dc = self.dc.into_push_pull_output();
         let reset = self.reset.into_push_pull_output();
 
+        let spi = SpiDeviceWrapper::new(spi, cs);
         // Create a SPIInterface over the peripheral, then create the ILI9341 driver
         // using said interface and set its default orientation.
-        let interface = SPIInterface::new(spi, dc, cs);
+        let interface = SPIInterface::new(spi, dc);
+        //  interface.send_command(0x01, &[]).ok();
         let ili9341 = Ili9341::new(
             interface,
             reset,
