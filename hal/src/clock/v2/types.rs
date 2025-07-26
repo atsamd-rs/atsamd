5033501f2a9cb1bdc97697181a36891f3d9de155
Ian Rees <code@ianrees.nz>
2025-07-26 20:09:54 +1200

    stash 4 - types

--- hal/src/clock/v2/types.rs
+++ hal/src/clock/v2/types.rs
@@ -21,30 +21,6 @@ use atsamd_hal_macros::hal_cfg;
 
 use crate::typelevel::Sealed;
 
-#[hal_cfg("sercom0")]
-pub use crate::sercom::Sercom0;
-
-#[hal_cfg("sercom1")]
-pub use crate::sercom::Sercom1;
-
-#[hal_cfg("sercom2")]
-pub use crate::sercom::Sercom2;
-
-#[hal_cfg("sercom3")]
-pub use crate::sercom::Sercom3;
-
-#[hal_cfg("sercom4")]
-pub use crate::sercom::Sercom4;
-
-#[hal_cfg("sercom5")]
-pub use crate::sercom::Sercom5;
-
-#[hal_cfg("sercom6")]
-pub use crate::sercom::Sercom6;
-
-#[hal_cfg("sercom7")]
-pub use crate::sercom::Sercom7;
-
 macro_rules! create_types {
     (
         $(
@@ -63,100 +39,172 @@ macro_rules! create_types {
     };
 }
 
-create_types!(Ac);
-create_types!(Adc0);
-#[hal_cfg("clock-d5x")]
-create_types!(Adc1);
-#[hal_cfg("clock-d5x")]
-create_types!(Aes);
-#[hal_cfg("can0")]
-create_types!(Can0);
-#[hal_cfg("can1")]
-create_types!(Can1);
-#[hal_cfg("clock-d5x")]
-create_types!(Ccl);
-#[hal_cfg("clock-d5x")]
-create_types!(Cmcc);
-#[hal_cfg("clock-d5x")]
-create_types!(CM4Trace);
-create_types!(Dac);
+// AHB types
 create_types!(Dmac);
 create_types!(Dsu);
-create_types!(Eic);
-create_types!(
-    EvSys, EvSys0, EvSys1, EvSys2, EvSys3, EvSys4, EvSys5, EvSys6, EvSys7, EvSys8, EvSys9, EvSys10,
-    EvSys11
-);
-#[hal_cfg("clock-d5x")]
-create_types!(FreqM);
-#[hal_cfg("clock-d5x")]
-create_types!(FreqMMeasure);
-#[hal_cfg("clock-d5x")]
-create_types!(FreqMReference);
-create_types!(Gclk);
-#[hal_cfg("gmac")]
-create_types!(Gmac);
 create_types!(Hpb0, Hpb1, Hpb2);
-#[hal_cfg("clock-d5x")]
-create_types!(Hpb3);
-#[hal_cfg("clock-d5x")]
-create_types!(Icm);
-#[hal_cfg("clock-d5x")]
-create_types!(Mclk);
 create_types!(NvmCtrl);
-#[hal_cfg("clock-d5x")]
-create_types!(NvmCtrlSmeeProm, NvmCtrlCache);
-#[hal_cfg("clock-d5x")] // TODO was min-samd51j in Bradley's work
-create_types!(I2S, I2S0, I2S1);
-#[hal_cfg("clock-d5x")]
-create_types!(OscCtrl);
-#[hal_cfg("clock-d5x")]
-create_types!(Osc32kCtrl);
+create_types!(Usb);
+
+// APB types
+create_types!(Ac);
+create_types!(Adc0);
+create_types!(Dac);
+create_types!(Eic);
+create_types!(EvSys);
+create_types!(Gclk);
+#[hal_cfg("i2s")]
+create_types!(I2S);
 create_types!(Pac0);
-#[hal_cfg(any("clock-d11", "clock-d21"))]
-create_types!(Pac1, Pac2);
-#[hal_cfg("clock-d5x")]
-create_types!(Pcc);
-#[hal_cfg("clock-d5x")]
-create_types!(PDec);
 create_types!(Pm);
 create_types!(Port);
-#[hal_cfg("clock-d5x")]
-create_types!(Pukcc);
-#[hal_cfg("clock-d5x")]
-create_types!(Qspi, Qspi2x);
-#[hal_cfg("clock-d5x")]
-create_types!(RamEcc);
-#[hal_cfg("clock-d5x")]
-create_types!(RstC);
 create_types!(Rtc);
-#[hal_cfg("clock-d5x")]
-create_types!(Sdhc0);
-#[hal_cfg("sdhc1")]
-create_types!(Sdhc1);
-create_types!(SlowClk);
-#[hal_cfg("clock-d5x")]
-create_types!(SupC);
-#[hal_cfg(any("clock-d11", "clock-d21"))]
-create_types!(SysCtrl);
-#[hal_cfg("clock-d5x")]
-create_types!(Tc0Tc1, Tc0, Tc1);
-#[hal_cfg("clock-d5x")]
-create_types!(Tc2Tc3, Tc2, Tc3);
-#[hal_cfg(all("tc4", "tc5"))]
-create_types!(Tc4Tc5, Tc4, Tc5);
-#[hal_cfg(all("tc6", "tc7"))]
-create_types!(Tc6Tc7, Tc6, Tc7);
-create_types!(Tcc0Tcc1, Tcc0, Tcc1);
-#[hal_cfg("clock-d5x")]
-create_types!(Tcc2Tcc3, Tcc2);
+#[hal_cfg("sercom0")]
+pub use crate::sercom::Sercom0;
+#[hal_cfg("sercom1")]
+pub use crate::sercom::Sercom1;
+#[hal_cfg("sercom2")]
+pub use crate::sercom::Sercom2;
+#[hal_cfg("sercom3")]
+pub use crate::sercom::Sercom3;
+#[hal_cfg("sercom4")]
+pub use crate::sercom::Sercom4;
+#[hal_cfg("sercom5")]
+pub use crate::sercom::Sercom5;
+#[hal_cfg("sercom6")]
+pub use crate::sercom::Sercom6;
+#[hal_cfg("sercom7")]
+pub use crate::sercom::Sercom7;
+
+#[hal_cfg("tc0")]
+create_types!(Tc0);
+#[hal_cfg("tc1")]
+create_types!(Tc1);
+#[hal_cfg("tc2")]
+create_types!(Tc2);
+#[hal_cfg("tc3")]
+create_types!(Tc3);
+#[hal_cfg("tc4")]
+create_types!(Tc4);
+#[hal_cfg("tc5")]
+create_types!(Tc5);
+#[hal_cfg("tc6")]
+create_types!(Tc6);
+#[hal_cfg("tc7")]
+create_types!(Tc7);
+
+#[hal_cfg("tcc0")]
+create_types!(Tcc0);
+#[hal_cfg("tcc1")]
+create_types!(Tcc1);
+#[hal_cfg("tcc2")]
+create_types!(Tcc2);
 #[hal_cfg("tcc3")]
 create_types!(Tcc3);
 #[hal_cfg("tcc4")]
 create_types!(Tcc4);
-#[hal_cfg(any("clock-d11", "clock-d21"))]
-create_types!(Tcc2Tc3, Tcc2, Tc3);
-#[hal_cfg("clock-d5x")]
-create_types!(Trng);
-create_types!(Usb);
+
+// TODO
+#[hal_cfg(all("tc0", "tc1"))]
+create_types!(Tc0Tc1);
+#[hal_cfg(all("tc2", "tc3"))]
+create_types!(Tc2Tc3);
+#[hal_cfg(all("tc4", "tc5"))]
+create_types!(Tc4Tc5);
+#[hal_cfg(all("tc6", "tc7"))]
+create_types!(Tc6Tc7);
+
+#[hal_cfg(all("tcc0", "tcc1"))]
+create_types!(Tcc0Tcc1);
+#[hal_cfg(all("tcc2", "tcc3"))]
+create_types!(Tcc2Tcc3);
+
+#[hal_cfg(all("tcc2", "tc3-d21"))]
+create_types!(Tcc2Tc3);
+
 create_types!(Wdt);
+
+// PCLK types
+// TODO set channel numbers in devices.yaml
+create_types!(
+    EvSys0, EvSys1, EvSys2, EvSys3, EvSys4, EvSys5, EvSys6, EvSys7, EvSys8, EvSys9, EvSys10,
+    EvSys11
+);
+#[hal_cfg("i2s")]
+create_types!(I2S0, I2S1);
+create_types!(SlowClk);
+
+#[hal_cfg("clock-d5x")]
+mod variant_ahb_types {
+    use super::{Sealed, hal_cfg};
+
+    #[hal_cfg("can0")]
+    create_types!(Can0);
+    #[hal_cfg("can1")]
+    create_types!(Can1);
+    create_types!(Cmcc);
+    #[hal_cfg("gmac")]
+    create_types!(Gmac);
+    create_types!(Hpb3);
+    create_types!(Icm);
+    create_types!(NvmCtrlSmeeProm, NvmCtrlCache);
+    create_types!(Pukcc);
+    create_types!(Qspi, Qspi2x);
+    #[hal_cfg("sdhc0")]
+    create_types!(Sdhc0);
+    #[hal_cfg("sdhc1")]
+    create_types!(Sdhc1);
+}
+
+#[hal_cfg("clock-d5x")]
+mod variant_apb_types {
+    use super::Sealed;
+
+    create_types!(Adc1);
+    create_types!(Aes);
+    create_types!(Ccl);
+    create_types!(FreqM);
+    create_types!(Mclk);
+    create_types!(OscCtrl);
+    create_types!(Osc32kCtrl);
+    create_types!(Pcc);
+    create_types!(PDec);
+    create_types!(RamEcc);
+    create_types!(RstC);
+    create_types!(SupC);
+    create_types!(Trng);
+}
+
+#[hal_cfg("clock-d5x")]
+mod variant_pclk_types {
+    use super::Sealed;
+
+    create_types!(CM4Trace);
+    create_types!(FreqMMeasure);
+    create_types!(FreqMReference);
+}
+
+#[hal_cfg(any("clock-d11", "clock-d21"))]
+mod variant_apb_types {
+    use super::{Sealed, hal_cfg};
+
+    #[hal_cfg("clock-d21")]
+    create_types!(Ac1);
+    create_types!(Pac1, Pac2);
+    create_types!(Ptc);
+    create_types!(SysCtrl);
+}
+
+#[hal_cfg(any("clock-d11", "clock-d21"))]
+mod variant_pclk_types {
+    use super::Sealed;
+
+    create_types!(AcDig);
+    create_types!(AcAna);
+    create_types!(SercomSlow);
+}
+
+#[hal_cfg("clock-d5x")]
+pub use variant_ahb_types::*;
+pub use variant_apb_types::*;
+pub use variant_pclk_types::*;
