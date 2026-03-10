621fb6e373de8e92a398e3ac34715bf785924b6b
Ashcon Mohseninia <ashconm@outlook.com>
2026-03-10 11:09:26 +0000

    Indent fixes

--- hal/src/peripherals/dsu.rs
+++ hal/src/peripherals/dsu.rs
@@ -237,16 +237,18 @@ impl Dsu {
     /// ## Algorithm:
     /// 1. Write entire memory to '0', in any order.
     /// 2. Bit by bit read '0', write '1', in descending order.
-    /// 3. Bit by bit read '1', write '0', read '0', write '1', in ascending order.
+    /// 3. Bit by bit read '1', write '0', read '0', write '1', in ascending
+    ///    order.
     /// 4. Bit by bit read '1', write '0', in ascending order.
-    /// 5. Bit by bit read '0', write '1', read '1', write '0', in ascending order.
+    /// 5. Bit by bit read '0', write '1', read '1', write '0', in ascending
+    ///    order.
     /// 6. Read '0' from entire memory, in ascending order.
     ///
     ///
     /// - `address` is an address within the CPUs RAM space; must be
     ///   word-aligned
-    /// - `length` is a length of memory region that is being tested.
-    ///   Must be word-aligned
+    /// - `length` is a length of memory region that is being tested. Must be
+    ///   word-aligned
     pub unsafe fn memory_test(&mut self, address: u32, length: u32) -> Result<()> {
         if address % 4 != 0 {
             return Err(Error::AlignmentError);
