06a9c443a18614da3a009bd5221d2879181eda72
Ashcon Mohseninia <ashconm@outlook.com>
2026-03-20 22:36:42 +0100

    Timer - Fix misuse of ctrlbset and ctrlbclr

--- hal/src/peripherals/timer/d11.rs
+++ hal/src/peripherals/timer/d11.rs
@@ -57,11 +57,11 @@ where
         // need to manually read the bit here
         while count.ctrla().read().bits() & 1 != 0 {}
 
-        count.ctrlbset().write(|w| {
+        count.ctrlbclr().write(|w| {
             // Count up when the direction bit is zero
-            w.dir().clear_bit();
+            w.dir().set_bit();
             // Periodic
-            w.oneshot().clear_bit()
+            w.oneshot().set_bit()
         });
 
         // Set TOP value for mfrq mode
