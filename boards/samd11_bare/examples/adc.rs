73e2fe28502e42a518d9299e083810b559ac917f
Ashcon Mohseninia <ashconm@outlook.com>
2026-06-02 19:53:03 +0100

    Fix samd11_bare ADC example

--- boards/samd11_bare/examples/adc.rs
+++ boards/samd11_bare/examples/adc.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 
-use atsamd_hal::adc::{AdcBuilder, Reference};
+use atsamd_hal::adc::AdcBuilder;
+use atsamd_hal::clock::v2::clock_system_at_reset;
+use atsamd_hal::clock::v2::pclk::Pclk;
 use samd11_bare as bsp;
 
 use bsp::hal;
@@ -17,38 +19,32 @@ use bsp::Pins;
 use pac::{CorePeripherals, Peripherals};
 
 use hal::{
-    adc::{Accumulation, Adc, Prescaler, Resolution},
-    clock::GenericClockController,
+    adc::{Accumulation, Prescaler},
 };
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = Peripherals::take().unwrap();
+    let peripherals = Peripherals::take().unwrap();
     let _core = CorePeripherals::take().unwrap();
 
     let pins = Pins::new(peripherals.port);
 
-    let mut clocks = GenericClockController::with_external_32kosc(
-        peripherals.gclk,
-        &mut peripherals.pm,
-        &mut peripherals.sysctrl,
-        &mut peripherals.nvmctrl,
-    );
-    let gclk0 = clocks.gclk0();
-    let adc_clock = clocks.adc(&gclk0).unwrap();
 
+    let (_buses, clocks, tokens) = clock_system_at_reset(peripherals.gclk, peripherals.pm, peripherals.sysctrl);
+    let (adc_pclk, _gclk0) = Pclk::enable(tokens.pclks.adc, clocks.gclk0);
+    let adc_apb = clocks.apbs.adc0;
+    
     let mut adc = AdcBuilder::new(Accumulation::single(atsamd_hal::adc::AdcResolution::_12))
         .with_clock_cycles_per_sample(5)
         .with_clock_divider(Prescaler::Div128)
-        .with_vref(Reference::Arefa)
-        .enable(peripherals.adc, &mut peripherals.pm, &adc_clock)
+        .with_vref(atsamd_hal::adc::Reference::Intvcc0)
+        .enable(peripherals.adc, adc_apb, &adc_pclk)
         .unwrap();
-
-    let mut adc_pin = pins.d1.into_alternate();
+    let mut adc_pin = pins.a0.into_alternate();
 
     loop {
         let res = adc.read(&mut adc_pin);
         #[cfg(feature = "use_semihosting")]
-        cortex_m_semihosting::hprintln!("ADC Result: {}", res);
+        cortex_m_semihosting::hprintln!("ADC value: {}", read).unwrap();
     }
 }
