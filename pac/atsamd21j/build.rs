5c35c426aa67e9325a87cce4c0a1aa8fc0301f01
Justin Beaurivage <code@beaurivage.io>
2026-08-09 23:03:12 -0400

    Fix linker script path in build scripts

--- pac/atsamd21j/build.rs
+++ pac/atsamd21j/build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = env::var("OUT_DIR").unwrap();
     let svd_root = Path::new("../../svd");
     let pkg_name = env!("CARGO_PKG_NAME");
-    let linker_script_path = Path::new("./memory.x");
+    let linker_script_path = Path::new("./device.x");
 
     atsamd_hal_pac_builder::generate_pac(pkg_name, &out_dir, svd_root)?;
     atsamd_hal_pac_builder::include_linker_script(&out_dir, linker_script_path)?;
